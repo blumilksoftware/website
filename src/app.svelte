3fcc880fe09436dd144b37487f11319a6fdc61ed
Krzysztof Rewak <krzysztof.rewak@gmail.com>
2020-11-08 09:35:42 +0100

    #10 - moving Tailwind configuration to rollup

--- src/app.svelte
+++ src/app.svelte
@@ -6,3 +6,16 @@
   import { routes } from '@sveltech/routify/tmp/routes'
   import Bubbles from './layout/bubbles.svelte'
 </script>
+
+<style global>
+    @tailwind base;
+    @tailwind components;
+    @tailwind utilities;
+
+    body {
+        background: #f4f8fd;
+        display: flex;
+        min-height: 100vh;
+        flex-direction: column;
+    }
+</style>
