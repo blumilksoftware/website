43a6fb68049819fbe73f8f1b08bbebeccc83fa34
Krzysztof Rewak <krzysztof.rewak@gmail.com>
2020-11-08 09:52:54 +0100

    #10 - added SRI to icons

--- src/app.svelte
+++ src/app.svelte
@@ -11,11 +11,4 @@
     @tailwind base;
     @tailwind components;
     @tailwind utilities;
-
-    body {
-        background: #f4f8fd;
-        display: flex;
-        min-height: 100vh;
-        flex-direction: column;
-    }
 </style>
