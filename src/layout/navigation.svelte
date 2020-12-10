d2e6b62e5ffdc6c870be95280bfbefe2bda1502a
Krzysztof Rewak <krzysztof.rewak@gmail.com>
2020-12-10 18:20:53 +0100

    #7 - redirect to Google forms

--- src/layout/navigation.svelte
+++ src/layout/navigation.svelte
@@ -1,4 +1,4 @@
-<div class="container mx-auto mt-6 mb-24 flex items-center">
+<div class="container mx-auto mt-6 mb-36 flex items-center">
     <div class="text-2xl">
         <a href="{ $url('/') }" class="flex items-center">
             <img src="./logo.png" alt="Blumilk" class="w-12 mr-3">
--- src/pages/contact.svelte
+++ src/pages/contact.svelte
@@ -1,19 +1,20 @@
 <div class="container mx-auto flex">
     <div class="flex-1">
         <ContentSection title="Contact us">
-            <form>
+            <form target="_blank" id="contact" action="{ url }">
                 <div class="px-12 py-4">
-                    <label for="{{ emailName}}" class="block text-gray-700">E-mail address</label>
-                    <input type="text" name="{{ emailName}}" id="{{ emailName}}" placeholder="E-mail address"
+                    <label for="{ emailName }" class="block text-gray-700">E-mail address</label>
+                    <input type="text" name="{ emailName }" id="{ emailName }" placeholder="E-mail address"
                            class="px-4 py-2 mt-1 block w-full shadow-sm border-gray-300 rounded-md">
                 </div>
                 <div class="px-12 py-4">
-                    <label for="{{ questionName }}" class="block text-gray-700">Message</label>
-                    <textarea rows="8" name="{{ questionName }}" id="{{ questionName }}" placeholder="Message"
+                    <label for="{ questionName }" class="block text-gray-700">Message</label>
+                    <textarea rows="8" name="{ questionName }" id="{ questionName }" placeholder="Message"
                               class="px-4 py-2 mt-1 block w-full shadow-sm border-gray-300 rounded-md"></textarea>
                 </div>
                 <div class="px-12 py-4">
-                    <div class="text-center px-4 py-2 mt-1 block w-full rounded-md bg-blue-500 text-white">
+                    <div on:click={handleFormSubmit}
+                         class="text-center px-4 py-2 mt-1 block w-full rounded-md bg-blue-500 text-white cursor-pointer">
                         Send with <i class="fitted google icon"></i> Forms
                     </div>
                 </div>
@@ -21,9 +22,14 @@
         </ContentSection>
 
         <ContentSection title="Visit us">
-            <p class="text-lg text-justify">
+            <p class="text-lg text-justify px-8 py-2">
                 We are working from the office located in the heart of Legnica, Poland.
-                The railway station is less than 10 minutes by foot.
+                In the closest neighbourhood you can dine in nice restaurants, grab a beer in local pubs or shop in
+                shopping malls.
+                The railway station is less than 10 minutes by foot. City park is even closer.
+            </p>
+            <p class="text-lg text-justify px-8 py-2">
+                Our address is Najświętszej Marii Panny 5F/5.
             </p>
         </ContentSection>
     </div>
@@ -35,7 +41,11 @@
 <script>
   import ContentSection from '../components/section.svelte'
 
+  function handleFormSubmit () {
+    document.getElementById('contact').submit()
+  }
+
   export let url = global.env.CONTACT_FORM_URL + '/formResponse'
-  export let emailName = global.env.CONTACT_FORM_EMAIL_NAME + '/formResponse'
-  export let questionName = global.env.CONTANT_FORM_QUESTION_NAME + '/formResponse'
+  export let emailName = global.env.CONTACT_FORM_EMAIL_NAME
+  export let questionName = global.env.CONTANT_FORM_QUESTION_NAME
 </script>
