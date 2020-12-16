2ec4ebe867f163f20a6a9b6122f995279f3264cc
Krzysztof Rewak <krzysztof.rewak@gmail.com>
2020-12-16 15:09:25 +0100

    #20 - GA added

--- src/main.js
+++ src/main.js
@@ -3,6 +3,13 @@ import { addMessages, getLocaleFromNavigator, init } from 'svelte-i18n'
 import en from './locale/en.json'
 import pl from './locale/pl.json'
 
+window.dataLayer = window.dataLayer || []
+
+function gtag () {dataLayer.push(arguments)}
+
+gtag('js', new Date())
+gtag('config', 'G-Y3WCJ1ZM46')
+
 const app = new App({
   target: document.body
 })
