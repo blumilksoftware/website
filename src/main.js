4aebc3a540ba4595387a73fec6ed2b3a13e2d843
EwelinaLasowy <ewelina.lasowy@blumilk.pl>
2021-10-06 09:29:54 +0200

    #85 - disable Google Analytics in local environment

--- src/main.js
+++ src/main.js
@@ -4,14 +4,19 @@ import { addMessages, getLocaleFromNavigator, init } from 'svelte-i18n'
 import en from './locale/en.json'
 import pl from './locale/pl.json'
 
-window.dataLayer = window.dataLayer || []
+if (env['APP_ENV'] === 'local') {
+  const disableGA = 'ga-disable-' + env['G_TAG']
+  window[disableGA] = true
+} else {
+  window.dataLayer = window.dataLayer || []
 
-function gtag () {
-  window.dataLayer.push(arguments)
+  function gtag () {
+    window.dataLayer.push(arguments)
+  }
+
+  gtag('js', new Date())
+  gtag('config', env['G_TAG'])
 }
-
-gtag('js', new Date())
-gtag('config', 'G-Y3WCJ1ZM46')
 
 const app = new App({
   target: document.body
