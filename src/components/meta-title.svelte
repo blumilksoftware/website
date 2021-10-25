e9373c17a227f7c8c73d1d7565240f66fdb033e2
Kamil Piech <kamil.piech.97@gmail.com>
2021-10-25 22:17:49 +0200

    #96 - added og tags.

--- src/components/meta-title.svelte
+++ src/components/meta-title.svelte
@@ -1,9 +1,14 @@
 <svelte:head>
     <title>{title} | Blumilk </title>
     <meta name="description" content="{description}" />
+    <meta property="og:title" content="{title} | Blumilk" />
+    <meta property="og:description" content="{description}" />
+    <meta property="og:photo" content="https://www.blumilk.pl/logo.svg" />
+    <meta property="og:type" content="website" />
 </svelte:head>
 
 <script>
   export let title = 'Blumilk Software Development'
   export let description = 'Software tailored precisely for your needs. We are a team of skilled software engineers specialized in web applications and research and development projects.'
 </script>
+
