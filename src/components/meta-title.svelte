fe6aacb6d6eb98879a7b0aa9f974284716fa15eb
Kamil Piech <kamil.piech@blumilk.pl>
2021-10-26 12:02:59 +0200

    #96 - og tags (#125)
    * #96 - start working with og tags.
    
    * #96 - added og tags.
    
    * #96 - added twitter tags.
    
    * #96 - fix.
    
    * #96 - added search console verification.

--- src/components/meta-title.svelte
+++ src/components/meta-title.svelte
@@ -1,9 +1,17 @@
 <svelte:head>
     <title>{title} | Blumilk </title>
     <meta name="description" content="{description}" />
+    <meta property="og:title" content="{title} | Blumilk" />
+    <meta property="og:description" content="{description}" />
+    <meta property="og:photo" content="https://www.blumilk.pl/logo.svg" />
+    <meta property="og:type" content="website" />
+    <meta name="twitter:card" content="summary" />
+    <meta name="twitter:site" content="@blumilksoftware" />
+    <meta name="twitter:creator" content="@blumilksoftware" />
 </svelte:head>
 
 <script>
   export let title = 'Blumilk Software Development'
   export let description = 'Software tailored precisely for your needs. We are a team of skilled software engineers specialized in web applications and research and development projects.'
 </script>
+
