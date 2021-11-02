1ba86e89fc408bb393c507dbea49b3237f32ff46
Krzysztof Rewak <krzysztof.rewak@gmail.com>
2021-11-02 08:13:33 +0100

    #97 - i18n refactor (WIP)

--- src/app.svelte
+++ src/app.svelte
@@ -1,10 +1,21 @@
 <Bubbles/>
-<Router {routes}/>
+<Router {routes} {config}/>
 
 <script>
   import { Router } from '@roxi/routify'
   import { routes } from '../.routify/routes'
   import Bubbles from '@/layout/bubbles.svelte'
+
+  const locales = ['en', 'pl']
+  const fragment = location.pathname.split('/')[1]
+  const locale = locales.includes(fragment) ? `/${fragment}` : ''
+
+  const config = {
+    urlTransform: {
+      apply: (url) => locale + url,
+      remove: (url) => url.replace(locale, ''),
+    },
+  }
 </script>
 
 <style global>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import App from './app.svelte'
-import { addMessages, getLocaleFromNavigator, init } from 'svelte-i18n'
+import { page, goto } from "@roxi/routify";
+import { addMessages, getLocaleFromNavigator, init, locale } from 'svelte-i18n'
 
 import en from './locale/en.json'
 import pl from './locale/pl.json'
@@ -8,6 +9,21 @@ window.dataLayer = window.dataLayer || []
 
 function gtag () {
   window.dataLayer.push(arguments)
+}
+
+function getLocale () {
+  const locales = ['en', 'pl']
+  const fragment = location.pathname.split('/')[1]
+  const prefix = locales.includes(fragment) ? fragment : ''
+
+  if(prefix) {
+    locale.set(prefix)
+    return prefix
+  } else {
+    goto(page.path);
+  }
+
+  return getLocaleFromNavigator()
 }
 
 gtag('js', new Date())
@@ -22,7 +38,7 @@ addMessages('pl', pl)
 
 init({
   fallbackLocale: 'en',
-  initialLocale: getLocaleFromNavigator()
+  initialLocale: getLocale()
 })
 
 export default app
