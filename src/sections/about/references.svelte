1b2fa931398e0812d40a747f0c7ef21686b139ea
Krzysztof Rewak <krzysztof.rewak@blumilk.pl>
2021-12-01 12:14:28 +0100

    #134 - removed some strange remnants

--- src/sections/about/references.svelte
+++ src/sections/about/references.svelte
@@ -23,20 +23,6 @@
     </div>
 </Section>
 
-{#if false}
-    <div class="mt-12 container mx-auto flex justify-evenly">
-        <div class="bg-white rounded-xl shadow-lg">
-            <img src="/images/photos/placeholder.jpg" alt="Team #1" class="w-96 p-3 relative z-10">
-        </div>
-        <div class="bg-white rounded-xl shadow-lg">
-            <img src="/images/photos/placeholder.jpg" alt="Team #2" class="w-96 p-3 relative z-10">
-        </div>
-        <div class="bg-white rounded-xl shadow-lg">
-            <img src="/images/photos/placeholder.jpg" alt="Team #3" class="w-96 p-3 relative z-10">
-        </div>
-    </div>
-{/if}
-
 <script>
   import { _ } from 'svelte-i18n'
   import Section from '@/components/lead-section.svelte'
