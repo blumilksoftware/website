4fcfe8b54c3f99ef0e42a1e9fd0ac9b47ccf3c38
Kamil Piech <kamil.piech@blumilk.pl>
2022-02-10 08:51:44 +0100

    #131 - partnerships page (#145)
    * #131 - wip
    
    * #131 - wip
    
    * #131 - wip
    
    * Apply suggestions from code review
    
    Co-authored-by: Ewelina Lasowy <56546832+EwelinaLasowy@users.noreply.github.com>
    
    * #131 - changes in language files and added better cwup logo
    
    * Apply suggestions from code review
    
    Co-authored-by: Ewelina Lasowy <56546832+EwelinaLasowy@users.noreply.github.com>
    
    * #131 - changes in language files
    
    * #131 - changes in language files
    
    * #131 - changes in language files
    
    * #131 - moving partners to assets
    
    * #131 - extended deploy script
    
    * Update src/locale/en.json
    
    Co-authored-by: Ewelina Lasowy <56546832+EwelinaLasowy@users.noreply.github.com>
    
    Co-authored-by: Ewelina Lasowy <56546832+EwelinaLasowy@users.noreply.github.com>
    Co-authored-by: Krzysztof Rewak <krzysztof.rewak@gmail.com>

---
+++ src/pages/partnerships.svelte
@@ -0,0 +1,23 @@
+<Meta title={ $_('title.partnerships') } description={ $_('description.partnerships') }/>
+
+{#each partners as partner, i}
+    <Section classes="{ i % 2 !== 0 ? 'lg:flex-row-reverse' : '' } lg:my-12">
+        <div slot="header">
+            { $_(partner.header) }
+        </div>
+        <div slot="description">
+            { $_(partner.lead) }
+        </div>
+        <div slot="content">
+            <img class="p-12 relative z-10 pointer-events-none w-4/5 sm:w-3/5 md:w-3/4 xl:w-1/2 m-auto" src="{ partner.logo }" alt="{ $_(partner.header) }" title="{ $_(partner.header) }">
+        </div>
+    </Section>
+{/each}
+
+<script>
+  import { _ } from 'svelte-i18n'
+
+  import partners from '@/assets/partners.js'
+  import Section from '@/components/lead-section.svelte'
+  import Meta from '@/components/meta-title.svelte'
+</script>
