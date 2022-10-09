548be704d84e0598a00a046c6c74a54d2040c38f
Krzysztof Rewak <krzysztof.rewak@gmail.com>
2022-10-09 18:55:16 +0200

    #171 - vite host for cypress

--- cypress.config.cjs
+++ cypress.config.cjs
@@ -4,20 +4,13 @@ module.exports = defineConfig({
   video: false,
   screenshotOnRunFailure: false,
   e2e: {
-    setupNodeEvents (on, config) {},
     specPattern: 'test/cypress/integration/**/*.cy.{js,jsx,ts,tsx}',
-    baseUrl: 'http://localhost:8080',
     supportFile: false,
   },
   component: {
     devServer: {
       framework: 'svelte',
       bundler: 'vite',
-      viteConfig: {
-        server: {
-          host: '127.0.0.1',
-        },
-      },
     },
   },
 })
