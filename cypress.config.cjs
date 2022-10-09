c3fa1d448689d38e45214155b20a6aef2b2b1989
Krzysztof Rewak <krzysztof.rewak@gmail.com>
2022-10-09 19:04:35 +0200

    #171 - default port

--- cypress.config.cjs
+++ cypress.config.cjs
@@ -4,7 +4,6 @@ module.exports = defineConfig({
   video: false,
   screenshotOnRunFailure: false,
   e2e: {
-    setupNodeEvents (on, config) {},
     specPattern: 'test/cypress/integration/**/*.cy.{js,jsx,ts,tsx}',
     baseUrl: 'http://localhost:5173',
     supportFile: false,
