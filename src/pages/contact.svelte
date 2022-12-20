487af5d5ce8a259c4dc2adffb787b9721c8594e8
EwelinaLasowy <ewelina.lasowy@blumilk.pl>
2022-12-20 10:03:57 +0100

    #183 - UX writing

--- src/pages/contact.svelte
+++ src/pages/contact.svelte
@@ -8,24 +8,23 @@
             { $_('pages.contact.lead') }
         </p>
         <form class="text-base relative z-10" target="_blank" id="contact" action="{ url }">
-            <div class="md:px-12 py-4">
-                <label for="{ questionName }" class="block text-gray-700"></label>
-                <textarea rows="8" name="{ questionName }" id="{ questionName }"
-                          placeholder="{ $_('pages.contact.message') }"
-                          class="px-4 py-2 mt-1 block w-full shadow-sm border-gray-300 rounded-md"
-                          data-cy="contact-form-question"></textarea>
-            </div>
-            <div class="md:flex">
-                <div class="flex-1 md:pl-12 md:pr-4 py-2">
-                    <label for="{ emailName }" class="block text-gray-700"></label>
+            <div class="md:px-4 py-4 space-y-4">
+                <div>
+                    <label for="{ emailName }" class="block text-left text-gray-700">{ $_('pages.contact.email') }</label>
                     <input type="text" name="{ emailName }" id="{ emailName }"
-                           placeholder="{ $_('pages.contact.email') }"
                            class="px-4 py-2 my-1 block w-full shadow-sm border-gray-300 rounded-md"
                            data-cy="contact-form-email">
                 </div>
-                <div class="flex-1 md:pr-12 md:pl-4 py-2">
+                <div>
+                    <label for="{ questionName }" class="block text-left text-gray-700">{ $_('pages.contact.message') }</label>
+                    <textarea rows="8" name="{ questionName }" id="{ questionName }"
+                              placeholder="{ $_('pages.contact.placeholder') }"
+                              class="px-4 py-2 mt-1 block w-full shadow-sm border-gray-300 rounded-md"
+                              data-cy="contact-form-question"></textarea>
+                </div>
+                <div class="md:flex md:justify-end">
                     <div on:click={ handleFormSubmit }
-                         class="text-center px-4 py-2 mt-1 block w-full rounded-md bg-brand text-white cursor-pointer">
+                         class="text-center px-4 py-2 mt-1 rounded-md bg-brand text-white cursor-pointer">
                         { $_('pages.contact.send') } <i class="fitted google icon"></i> Forms
                     </div>
                 </div>
