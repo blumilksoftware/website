c28f97827ec8e466fae3f545abb9b56b67872390
Kamil Piech <kamil.piech@blumilk.pl>
2024-04-11 07:57:58 +0200

    - cleaning out old admin panel (#232)
    - cleaning out old admin panel stuff

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,10 +12,6 @@ use Illuminate\Support\Facades\Route;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         using: function (): void {
-            Route::middleware("web")
-                ->prefix("dashboard")
-                ->group(base_path("routes/dashboard.php"));
-
             Route::localized(function (): void {
                 Route::middleware("web")
                     ->group(base_path("routes/web.php"));
