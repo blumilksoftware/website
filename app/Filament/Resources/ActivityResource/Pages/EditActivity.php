d0a6c2cf4edc414664917f898508dd4514744d82
Kamil Piech <kamil.piech@blumilk.pl>
2024-04-18 13:02:30 +0200

    #222 - activities crud (#230)
    * - added filament package and first crud - user resource
    
    * - cdf
    
    * #222 - activity crud with multilanguage
    
    * #222 - merged main
    
    * - fixes
    
    * #222 - activities crud update
    
    * #222 - added factory and seeder
    
    * - cr fix
    
    * - cr fix
    
    * #222 - cr fixes
    
    * - fixes
    
    * #222 - cr fix
    
    * #222 - cr fixes, added filters
    
    * #222 - cr fixes

---
+++ app/Filament/Resources/ActivityResource/Pages/EditActivity.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Blumilk\Website\Filament\Resources\ActivityResource\Pages;
+
+use Blumilk\Website\Filament\Resources\ActivityResource;
+use Filament\Resources\Pages\EditRecord;
+
+class EditActivity extends EditRecord
+{
+    protected static string $resource = ActivityResource::class;
+}
---
+++ database/factories/ActivityFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Database\Factories;
+
+use Blumilk\Website\Models\Activity;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Activity>
+ */
+class ActivityFactory extends Factory
+{
+    public function definition(): array
+    {
+        $locales = config("app.translatable_locales");
+
+        return [
+            "title" => $this->translations($locales, $this->faker->sentence),
+            "subtitle" => $this->translations($locales, $this->faker->sentence),
+            "description" => $this->translations($locales, $this->faker->paragraph),
+            "photo" => sprintf("%s/%s", Activity::PHOTOS_DIRECTORY, $this->faker->image("public/storage/activities", width: 240, height: 240, fullPath: false)),
+            "published" => $this->faker->boolean,
+            "published_at" => $this->faker->date,
+        ];
+    }
+}
