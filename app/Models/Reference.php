26a86091193378f99076803a59629770cdeb1e20
Kamil <kamil.piech.97@gmail.com>
2024-05-08 11:57:31 +0200

    #224 - added references to admin panel

---
+++ app/Models/Reference.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Blumilk\Website\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Spatie\Translatable\HasTranslations;
+
+/**
+ * @property string $creator_name
+ * @property array $description
+ * @property string $photo
+ * @property string $company
+ * @property bool $published
+ * @property string $associate_link
+ */
+class Reference extends Model
+{
+    use HasTranslations;
+    use HasFactory;
+
+    public const string PHOTOS_DIRECTORY = "references";
+
+    public $translatable = [
+        "description",
+    ];
+    protected $fillable = [
+        "creator_name",
+        "photo",
+        "company",
+        "description",
+        "published",
+        "associate_link",
+    ];
+    protected $casts = [
+        "creator_name" => "array",
+        "description" => "array",
+        "published" => "boolean",
+    ];
+}
