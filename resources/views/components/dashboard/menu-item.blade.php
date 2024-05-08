202592dff389c9b80cbf902a9aabb174b2eb9e91
Aleksandra Kozubal <aleksandra.kozubal@studenci.collegiumwitelona.pl>
2024-05-08 08:40:13 +0200

    cr changes

--- resources/views/components/dashboard/menu-item.blade.php
+++
@@ -1,5 +0,0 @@
-<li>
-    <a href="#" class="@if(isset($active) && $active) bg-gray-50 @else hover:text-indigo-600 hover:bg-gray-50 @endif text-indigo-600 group flex gap-x-3 rounded-md px-4 py-2 text-sm leading-6 font-semibold">
-        {{ $label }}
-    </a>
-</li>
--- resources/views/dashboard/dashboard.blade.php
+++
@@ -1,5 +0,0 @@
-@extends("layout.dashboard")
-
-@section("content")
-    :)
-@endsection
