72419fe3afa4f1ec07a6daa410f7cc971d2384e8
Aleksandra Kozubal <aleksandra.kozubal@studenci.collegiumwitelona.pl>
2024-05-09 14:21:29 +0200

    change padding,lang

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -35,7 +35,7 @@
             </div>
         </div>
 
-        <section class="flex flex-col-reverse lg:flex-row py-24 px-[10%] md:px-[15%]">
+        <section class="flex flex-col-reverse lg:flex-row py-24 px-[10%] md:px-[20%]">
             <div class="flex basis-1/2 relative flex-col self-center gap-5">
                 <div class="text-3xl md:text-4xl lg:text-6xl text-center lg:justify-start lg:text-start py-4">
                     <h2 class="font-semibold text-gray-900 animation-appear">{{__("content.section_1.title_1")}}
@@ -46,7 +46,7 @@
                 <div class="flex flex-col items-center lg:items-start animation-appear">
                     <p class="text-xl lg:text-2xl font-light text-center lg:text-start">
                         {{__("content.section_1.subtitle_1")}}
-                        <span class="font-medium">{{__("content.section_1.subtitle_2")}}</span>
+                        <span class="font-medium leading-relaxed">{{__("content.section_1.subtitle_2")}}</span>
                     </p>
                     <div class="my-6">
                         <x-primary-button href="">{{ __("content.pages.about") }}</x-primary-button>
@@ -60,7 +60,7 @@
             </div>
         </section>
 
-        <section class="text-center px-[10%] md:px-[15%]">
+        <section class="text-center px-[10%] md:px-[20%]">
             <div class="text-2xl lg:text-3xl font-medium text-gray-400">{{ __("content.section_2.title_1") }}<span
                     class="text-website-normal">{{ __("content.section_2.title_2") }}</span>
             </div>
@@ -84,14 +84,14 @@
         <section class="bg-gradient-to-br from-gray-light to-white">
 
             <x-waves.wave-1.top></x-waves.wave-1.top>
-            <div class="flex flex-col lg:flex-row px-[10%] md:px-[15%] lg:pr-0 pt-24">
+            <div class="flex flex-col lg:flex-row px-[10%] md:px-[20%] lg:pr-0 pt-24">
                 <div
                     class="flex basis-2/5 flex-col lg:pr-8 delay-300 duration-700 taos:translate-x-48 taos:opacity-0"
                     data-taos-offset="100">
                     <h2 class="text-3xl md:text-4xl lg:text-6xl font-semibold text-gray-900 mb-4 text-center md:text-left">
                         {{__("content.section_3.title_1")}}</h2>
                     <div class="text-black text-xl text-center md:text-left py-6">
-                        <div class="pb-4 md:pb-0"> {{__("content.section_3.subtitle_1")}}
+                        <div class="pb-4 md:pb-0 leading-relaxed"> {{__("content.section_3.subtitle_1")}}
                             <span class="font-medium">
                                 {{__("content.section_3.subtitle_2")}}
                                 </span>
@@ -127,7 +127,7 @@
                                 <div
                                     class="flex flex-col items-center w-auto h-auto text-center bg-gray-light p-6 rounded-2xl">
                                     <img src="graphics/stars.png" class="max-h-12 shrink m-4" alt="5 stars"/>
-                                    <div class="text-xl">
+                                    <div class="text-xl leading-relaxed">
                                         {{__("content.section_3.subtitle_2")}}
                                     </div>
 
@@ -143,7 +143,7 @@
             <x-waves.wave-1.bottom></x-waves.wave-1.bottom>
         </section>
         <section
-            class="relative flex flex-col lg:flex-row md:py-24 px-[10%] md:px-[15%] lg:pr-0 items-center overflow-hidden">
+            class="relative flex flex-col lg:flex-row md:py-24 px-[10%] md:px-[20%] lg:pr-0 items-center overflow-hidden">
             <div class="flex basis-1/3 relative">
                 <div class="flex flex-col text-center md:text-left">
                     <div class="relative duration-700 taos:translate-y-48 taos:opacity-0"
@@ -151,18 +151,18 @@
                         <x-badge>{{__("content.section_4.badge")}}</x-badge>
                         <h2 class="text-3xl md:text-4xl lg:text-6xl font-semibold text-gray-900">{{__("content.section_4.title_1")}}</h2>
                     </div>
-                    <span class="absolute -right-[15%] -z-10">
+                    <span class="absolute -right-[20%] -z-10">
                             <x-bubble size="40"/>
                         </span>
                     <div class="text-black text-xl relative py-4">
                         <div class="py-4 text-xl duration-700 taos:translate-y-48 taos:opacity-0"
                              data-taos-offset="200"> {{__("content.section_4.subtitle_1")}}
                             <span
-                                class="font-medium">{{__("content.section_4.subtitle_2")}}</span></div>
+                                class="font-medium leading-relaxed">{{__("content.section_4.subtitle_2")}}</span></div>
                         <ul class="list-disc-none py-8 hidden md:block duration-1000 taos:translate-y-48 taos:opacity-0"
                             data-taos-offset="100">
                             @foreach($caseStudy as $key => $description)
-                                <li><span class="font-semibold">{{ $key }}</span> : {{ $description }}</li>
+                                <li><span class="font-semibold leading-relaxed">{{ $key }}</span> : {{ $description }}</li>
                             @endforeach
                         </ul>
                         <div class="relative pb-8">
@@ -185,7 +185,7 @@
                 <span class="absolute -left-[40%] top-72 -z-10">
                     <x-bubble size="90"/>
                 </span>
-                <span class="absolute -left-[25%] top-96 -z-10">
+                <span class="absolute -left-[20%] top-96 -z-10">
                     <x-bubble size="20"/>
                 </span>
             </div>
@@ -207,10 +207,10 @@
 
         <section class="relative py-20 text-center">
             <div class="hidden md:block bg-binary bg-cover bg-center bg-no-repeat object-contain min-w-full absolute inset-0"></div>
-            <div class="px-[10%] md:px-[15%] bg-gray-light py-8 md:py-24">
+            <div class="px-[10%] md:px-[20%] bg-gray-light py-8 md:py-24">
                 <h2 class="text-3xl md:text-5xl lg:text-6xl font-semibold text-gray-900 relative my-6 delay-300 duration-700 taos:scale-75 taos:opacity-0"
                     data-taos-offset="400">{{__("content.section_5.title_1")}}</h2>
-                <span class="subtitle relative delay-300 duration-700 taos:scale-75 taos:opacity-0"
+                <span class="font-medium leading-relaxed text-xl relative delay-300 duration-700 taos:scale-75 taos:opacity-0"
                       data-taos-offset="400">{{__("content.section_5.subtitle_1")}}</span>
                 <div
                     class="flex flex-col sm:flex-row relative text-gray-400 items-center my-6 md:justify-between lg:justify-center gap-y-8 sm:gap-x-8 lg:gap-x-16">
@@ -218,7 +218,7 @@
                         <img src="images/tech/laravel.svg" class="tech-icon h-full w-20 grayscale hover:grayscale-0 object-contain transform transition hover:scale-110" alt="laravel"/>
                     </div>
                     <div class="delay-300 duration-700 taos:scale-75 taos:opacity-0" data-taos-offset="300">
-                        <img src="images/tech/php.svg" class="tech-icon h-full w-20 grayscale hover:grayscale-0 object-contain transform transition hover:scale-110" alt="php"/>
+                        <img src="images/tech/php.svg" class="tech-icon h-full w-32 grayscale hover:grayscale-0 object-contain transform transition hover:scale-110" alt="php"/>
                     </div>
                     <div class="delay-500 duration-700 taos:scale-75 taos:opacity-0" data-taos-offset="300">
                         <img src="images/tech/postgresql.svg" class="tech-icon h-full w-20 grayscale hover:grayscale-0 object-contain transform transition hover:scale-110" alt="postgresql"/>
@@ -232,12 +232,12 @@
                 </div>
             </div>
         </section>
-        <section class="px-[10%] md:px-[15%] text-center items-center pb-36 delay-300 duration-700 taos:translate-y-48 taos:opacity-0 relative"
+        <section class="px-[10%] md:px-[20%] text-center items-center pb-36 delay-300 duration-700 taos:translate-y-48 taos:opacity-0 relative"
             data-taos-offset="300">
             <h2 class="text-3xl md:text-4xl lg:text-6xl font-semibold text-gray-900 mb-6">{{__("content.section_6.title_1")}}</h2>
             <x-badge>{{__("content.section_6.badge")}}</x-badge>
             <span class="subtitle">{{__("content.section_6.subtitle_1")}}<span
-                    class="font-semibold">{{__("content.section_6.subtitle_2")}}</span> {{__("content.section_6.subtitle_3")}}</span>
+                    class="font-semibold leading-relaxed text-xl">{{__("content.section_6.subtitle_2")}}</span> {{__("content.section_6.subtitle_3")}}</span>
             <div class="main-gallery js-flickity" data-flickity-options='{ "wrapAround": true }'>
                 <div class="gallery-cell gap-6 text-center py-4 lg:gap-12">
                     <x-testimonial
@@ -278,7 +278,7 @@
             </div>
         </section>
         <div class="md:py-20 block bg-binary bg-cover bg-center bg-no-repeat object-contain min-w-full inset-0">
-            <section class="relative px-[10%] md:px-[15%]">
+            <section class="relative px-[10%] md:px-[20%]">
 
                 <div class="relative flex flex-row items-center justify-between">
                     <h2 class="text-3xl md:text-4xl lg:text-6xl font-semibold text-gray-900">{{__("content.section_7.title_1")}}</h2>
