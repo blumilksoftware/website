de8db831e073b3377f60ea6d7ce2ae32895c7c91
Aleksandra Kozubal <aleksandra.kozubal@studenci.collegiumwitelona.pl>
2024-05-28 11:45:27 +0200

    cr changes

--- resources/views/components/icons/arrow.blade.php
+++ resources/views/components/icons/arrow.blade.php
@@ -1,3 +1,3 @@
-<div class="m-2 mr-8 bg-white p-1 rounded-full">
+<div class="m-2 mr-8 bg-white px-2 py-1 rounded-full">
     <i class="fa-solid fa-check text-website-normal fa-lg"></i>
 </div>
--- resources/views/layout/navigation.blade.php
+++ resources/views/layout/navigation.blade.php
@@ -3,75 +3,10 @@
     $current = substr(Route::currentRouteName(), 3);
 @endphp
 
-<nav id="mobile-nav"
-     class="sidebar bg-navigation hidden h-screen w-screen justify-between absolute z-30 right-0 md:hidden flex-col font-semibold text-xl sm:text-2xl text-start items-start">
-    <div class="flex justify-between relative bg-navigation p-8 px-14 w-full">
-        <a class="flex lowercase font-bold items-center" href="{{ route("home") }}">
-            <img class="w-32" src="{{asset('/logo.svg')}}" alt="Blumilk"/>
-        </a>
-        <button type="button" aria-label="Close sidebar"><i class="fa-solid fa-xmark text-2xl text-brand"
-                                                            onclick="hideSidebar()"></i></button>
-    </div>
-    <div class="w-full h-full px-14">
-        <button
-            class="w-full group hover:text-website-normal py-4 text-lg sm:text-xl text-start @if($current === "about") text-website-normal @endif"
-            onclick="window.location='{{ route("about") }}';">
-            <a class="my-4 p-2 start">{{ __("content.pages.about") }}</a>
-        </button>
-        <button
-            class="w-full group hover:text-website-normal py-4 text-lg sm:text-xl text-start @if($current === "case-studies") text-website-normal @endif"
-            onclick="window.location='{{ route("case-studies") }}';">
-            <a class="my-4 p-2">{{ __("content.pages.case_study") }}</a>
-        </button>
-        <button
-            class="w-full hover:text-website-normal py-4 text-lg sm:text-xl text-start @if($current === "partners") text-website-normal @endif"
-            onclick="window.location='{{ route("partners") }}';">
-            <a class="my-4 p-2">{{ __("content.pages.partners") }}</a>
-        </button>
-        <button
-            class="w-full group hover:text-website-normal py-4 text-lg sm:text-xl text-start @if($current === "career") text-website-normal @endif"
-            onclick="window.location='{{ route("career") }}';">
-            <a class="my-4 p-2">{{ __("content.pages.career") }}</a>
-        </button>
-        <button
-            class="w-full group hover:text-website-normal py-4 text-lg sm:text-xl text-start @if($current === "contact") text-website-normal @endif"
-            onclick="window.location='{{ route("contact") }}';">
-            <a class="my-4 p-2">{{ __("content.pages.contact") }}</a>
-        </button>
-        <div class="w-full px-2">
-            <div class="relative flex flex-col text-left">
-                @foreach(LocaleConfig::getLocales() as $locale)
-                    @if (App::isLocale($locale))
-                        <button type="button"
-                                class="flex w-full h-full justify-between items-center place-content-center gap-x-1.5 mt-4 text-lg sm:text-xl font-semibold text-black hover:text-website-normal uppercase"
-                                id="mobileLocalesButton" aria-expanded="true" aria-haspopup="true">
-                            {{ $locale }}
-                            <i class="fa-solid fa-chevron-up fa-rotate-180 langArrow text-gray-500"></i>
-                        </button>
-                    @endif
-                @endforeach
-                <div id="mobileLocalesMenu"
-                     class="hidden">
-                    <div class="text-end" role="none">
-                        @foreach(LocaleConfig::getLocales() as $locale)
-                            <a href="{{route(Route::currentRouteName(), Route::getCurrentRoute()->parameters(), true, $locale)}}"
-                               class="text-gray-500 block text-lg pt-4 sm:text-xl hover:text-website-normal uppercase text-start"
-                               role="menuitem" tabindex="-1">
-                                {{$locale}}
-                            </a>
-                        @endforeach
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="w-full pb-14 px-14">
-        <x-small-primary-button icon="text" href="#">{{ __("buttons.lets_talk") }}</x-small-primary-button>
-    </div>
-</nav>
-
-<nav id="desktop-nav" class="left-0 right-0 py-8 duration-300 xl:px-[20%] bg-navigation relative">
-    <div class="mx-auto flex items-center justify-between px-14 md:px-6 xl:px-0">
+<nav x-data="{ openLang: false, openMobileMenu: false}"
+     class="w-auto bg-navigation">
+    <div :class="{'block': ! openMobileMenu, 'hidden': openMobileMenu}"
+         class="mx-auto flex items-center justify-between px-14 md:px-6 left-0 right-0 py-8 xl:px-[20%] relative">
         <div>
             <a class="md:hidden tablet:block items-start" href="{{ route("home") }}">
                 <img class="w-32 shrink-0" src="{{asset('/logo.svg')}}" alt="Blumilk"/>
@@ -83,8 +18,7 @@
         <div class="md:flex hidden justify-center items-center text-sm 3xl:text-lg">
             <p class="font-medium flex items-center text-center gap-2">
                 <a href="{{route('about')}}"
-                   class="hover:bg-website-extra_light hover:text-website-normal rounded-3xl px-2 @if($current === "about") bg-website-extra_light text-website-normal @endif"
-                   href="{{ route("about") }}">{{ __("content.pages.about") }}</a>
+                   class="hover:bg-website-extra_light hover:text-website-normal rounded-3xl px-2 @if($current === "about") bg-website-extra_light text-website-normal @endif">{{ __("content.pages.about") }}</a>
                 <a href="{{ route("case-studies") }}"
                    class="hover:bg-website-extra_light hover:text-website-normal rounded-3xl px-2 @if($current === "case-studies") bg-website-extra_light text-website-normal @endif">{{ __("content.pages.case_study") }}</a>
                 <a href="{{ route("partners") }}"
@@ -103,20 +37,17 @@
                 @foreach(LocaleConfig::getLocales() as $locale)
                     @if (App::isLocale($locale))
                         <button type="button"
+                                @click="openLang = ! openLang"
                                 class="flex h-full items-center place-content-center gap-x-1.5 bg-white px-3 py-2 text-sm 3xl:text-md font-semibold text-gray-500 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 uppercase"
                                 id="desktopLocalesButton" aria-expanded="true" aria-haspopup="true">
                             {{ $locale }}
-                            <svg class="-mr-1 h-5 w-5 text-gray-400 langArrow" viewBox="0 0 20 20" fill="currentColor"
-                                 aria-hidden="true">
-                                <path fill-rule="evenodd"
-                                      d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
-                                      clip-rule="evenodd"/>
-                            </svg>
+                            <i class="fa-solid fa-chevron-up fa-rotate-180 langArrow text-gray-500"></i>
                         </button>
                     @endif
                 @endforeach
                 <div id="desktopLocalesMenu"
-                     class="hidden absolute right-0 z-10 mt-2 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+                     :class="{'block': openLang, 'hidden': ! openLang}"
+                     class="hidden absolute right-0 z-10 w-full mt-2 origin-top-right bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
                      tabindex="-1">
                     <div class="text-end" role="none">
                         @foreach(LocaleConfig::getLocales() as $locale)
@@ -131,66 +62,83 @@
             </div>
         </div>
 
-        <button class="flex md:hidden" onclick="showSidebar()">
-            <i class="fa-solid fa-bars hamburger-menu text-2xl text-brand"></i>
+        <button id="hamburgerMenu" class="flex md:hidden" @click="openMobileMenu = ! openMobileMenu">
+            <i :class="{'block': ! openMobileMenu, 'hidden': openMobileMenu}"
+               class="fa-solid fa-bars hamburger-menu text-2xl text-brand"></i>
+            <i :class="{'block': openMobileMenu, 'hidden': ! openMobileMenu}"
+               class="fa-solid fa-xmark text-2xl text-brand"></i>
         </button>
     </div>
+    <nav :class="{'block': openMobileMenu, 'hidden': ! openMobileMenu}"
+         class="w-full h-screen px-14 right-0 md:hidden flex flex-col justify-between font-semibold text-xl sm:text-2xl text-start items-start"
+         x-trap.noscroll="openMobileMenu">
+
+        <div class="w-full">
+            <div class="flex items-center justify-between left-0 right-0 py-8 xl:px-[20%] relative">
+                <a class="md:hidden tablet:block items-start" href="{{ route("home") }}">
+                    <img class="w-32 shrink-0" src="{{asset('/logo.svg')}}" alt="Blumilk"/>
+                </a>
+                <button class="flex md:hidden" @click="openMobileMenu = ! openMobileMenu">
+                    <i :class="{'block': openMobileMenu, 'hidden': ! openMobileMenu}"
+                       class="fa-solid fa-xmark text-2xl text-brand"></i>
+                </button>
+            </div>
+            <button
+                class="w-full group hover:text-website-normal py-4 text-lg sm:text-xl text-start @if($current === "about") text-website-normal @endif"
+                onclick="window.location='{{ route("about") }}';">
+                <a class="my-4 p-2 start">{{ __("content.pages.about") }}</a>
+            </button>
+            <button
+                class="w-full group hover:text-website-normal py-4 text-lg sm:text-xl text-start @if($current === "case-studies") text-website-normal @endif"
+                onclick="window.location='{{ route("case-studies") }}';">
+                <a class="my-4 p-2">{{ __("content.pages.case_study") }}</a>
+            </button>
+            <button
+                class="w-full hover:text-website-normal py-4 text-lg sm:text-xl text-start @if($current === "partners") text-website-normal @endif"
+                onclick="window.location='{{ route("partners") }}';">
+                <a class="my-4 p-2">{{ __("content.pages.partners") }}</a>
+            </button>
+            <button
+                class="w-full group hover:text-website-normal py-4 text-lg sm:text-xl text-start @if($current === "career") text-website-normal @endif"
+                onclick="window.location='{{ route("career") }}';">
+                <a class="my-4 p-2">{{ __("content.pages.career") }}</a>
+            </button>
+            <button
+                class="w-full group hover:text-website-normal py-4 text-lg sm:text-xl text-start @if($current === "contact") text-website-normal @endif"
+                onclick="window.location='{{ route("contact") }}';">
+                <a class="my-4 p-2">{{ __("content.pages.contact") }}</a>
+            </button>
+            <div class="w-full px-2">
+                <div class="relative flex flex-col text-left">
+                    @foreach(LocaleConfig::getLocales() as $locale)
+                        @if (App::isLocale($locale))
+                            <button type="button"
+                                    @click="openLang = ! openLang"
+                                    class="flex w-full h-full justify-between items-center place-content-center gap-x-1.5 mt-4 text-lg sm:text-xl font-semibold text-black hover:text-website-normal uppercase"
+                                    id="mobileLocalesButton" aria-expanded="true" aria-haspopup="true">
+                                {{ $locale }}
+                                <i class="fa-solid fa-chevron-up fa-rotate-180 langArrow text-gray-500"></i>
+                            </button>
+                        @endif
+                    @endforeach
+                    <div id="mobileLocalesMenu"
+                         :class="{'block': openLang, 'hidden': ! openLang}"
+                         class="hidden">
+                        <div class="text-end" role="none">
+                            @foreach(LocaleConfig::getLocales() as $locale)
+                                <a href="{{route(Route::currentRouteName(), Route::getCurrentRoute()->parameters(), true, $locale)}}"
+                                   class="text-gray-500 block text-lg pt-4 sm:text-xl hover:text-website-normal uppercase text-start"
+                                   role="menuitem" tabindex="-1">
+                                    {{$locale}}
+                                </a>
+                            @endforeach
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="w-full pb-14">
+            <x-small-primary-button icon="text" href="#">{{ __("buttons.lets_talk") }}</x-small-primary-button>
+        </div>
+    </nav>
 </nav>
-
-<script>
-    function showSidebar() {
-        const sidebar = document.querySelector(".sidebar");
-        sidebar.style.display = "flex";
-        document.body.style.overflow = 'hidden';
-    }
-
-    function hideSidebar() {
-        const sidebar = document.querySelector(".sidebar");
-        sidebar.style.display = "none";
-        document.body.style.overflow = '';
-    }
-    document.addEventListener('DOMContentLoaded', () => {
-        let dropdownButton;
-        let dropdownMenu;
-
-        function setDropdownElements() {
-            const isMobile = window.matchMedia('(max-width: 767px)').matches;
-            dropdownButton = document.getElementById(isMobile ? 'mobileLocalesButton' : 'desktopLocalesButton');
-            dropdownMenu = document.getElementById(isMobile ? 'mobileLocalesMenu' : 'desktopLocalesMenu');
-            setupDropdownButton();
-        }
-
-        function toggleDropdown() {
-            const langArrow = document.getElementsByClassName('langArrow');
-
-            if (dropdownMenu.classList.contains('hidden')) {
-                dropdownMenu.classList.remove('hidden');
-                setTimeout(() => {
-                    dropdownMenu.classList.remove('opacity-0', 'scale-95');
-                    dropdownMenu.classList.add('opacity-100', 'scale-100');
-                    for (let arrow of langArrow) {
-                        arrow.classList.add('rotate-0');
-                    }
-                }, 10);
-            } else {
-                dropdownMenu.classList.remove('opacity-100', 'scale-100');
-                dropdownMenu.classList.add('opacity-0', 'scale-95');
-                setTimeout(() => {
-                    dropdownMenu.classList.add('hidden');
-                    for (let arrow of langArrow) {
-                        arrow.classList.remove('rotate-0');
-                    }
-                }, 100);
-            }
-        }
-
-        function setupDropdownButton() {
-            dropdownButton.addEventListener('click', toggleDropdown);
-        }
-
-        setDropdownElements();
-        window.matchMedia('(max-width: 767px)').addEventListener('change', setDropdownElements);
-    });
-
-
-</script>
