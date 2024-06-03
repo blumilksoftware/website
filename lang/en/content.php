c0946c1a9b3327d3212b08a2c21460fe16933856
Aleksandra Kozubal <aleksandra.kozubal@studenci.collegiumwitelona.pl>
2024-06-03 13:08:32 +0200

    lint

--- lang/en/content.php
+++ lang/en/content.php
@@ -16,11 +16,11 @@ return [
             "title_2" => "tailored",
             "title_3" => "to your needs",
             "subtitle_1" => "We are passionate about creating web/mobile applications and advanced R&D projects.",
-            "subtitle_2" => "We turn concepts into working solutions."
+            "subtitle_2" => "We turn concepts into working solutions.",
         ],
         "section_2" => [
             "title_1" => "Join the ranks of ",
-            "title_2" => "satisfied Blumilk customers"
+            "title_2" => "satisfied Blumilk customers",
         ],
         "section_3" => [
             "title_1" => "How can we help you?",
@@ -31,41 +31,41 @@ return [
             "content_3" => "Business and process digitization",
             "content_4" => "Dedicated mobile and web applications",
             "title_2" => "We design solutions",
-            "subtitle_3" => "With a focus on hundred percent satisfaction of our clients and partners."
+            "subtitle_3" => "With a focus on hundred percent satisfaction of our clients and partners.",
         ],
         "section_4" => [
             "badge" => "see how we work",
             "title_1" => "Case Study",
             "subtitle_1" => "Learn how our implementations transformed clients' challenges into ",
-            "subtitle_2" => "effective solutions"
+            "subtitle_2" => "effective solutions",
         ],
         "section_5" => [
             "title_1" => "Tech Stack",
-            "subtitle_1" => "PHP and Laravel are our main tools. GitHub serves as our central platform, enabling code review and CI/CD through GitHub Actions. We build the frontend using Vue.js and utilize various database management systems, such as MySQL and PostgreSQL."
+            "subtitle_1" => "PHP and Laravel are our main tools. GitHub serves as our central platform, enabling code review and CI/CD through GitHub Actions. We build the frontend using Vue.js and utilize various database management systems, such as MySQL and PostgreSQL.",
         ],
         "section_6" => [
             "badge" => "reviews",
             "title_1" => "What others say about us",
             "subtitle_1" => "Our clients' opinions are very important, ",
             "subtitle_2" => "they attest to the quality of our services ",
-            "subtitle_3" => "and inspire us to constantly improve."
+            "subtitle_3" => "and inspire us to constantly improve.",
         ],
         "section_7" => [
-            "title_1" => "Our activities"
-        ]
+            "title_1" => "Our activities",
+        ],
     ],
     "about" => [
         "section_1" => [
             "title_1" => "Blumilk ",
             "title_2" => "is primarily people",
             "subtitle_1" => "Our team consists of over 25 unique technology enthusiasts - project managers, programmers, and testers with rich experiences. We serve both local and foreign clients, allowing us to flexibly adapt to diverse requirements and expectations.",
-            "subtitle_2" => "We are ready to meet even the most demanding projects."
+            "subtitle_2" => "We are ready to meet even the most demanding projects.",
         ],
         "section_2" => [
             "title_1" => "Our ",
             "title_2" => "mission",
             "subtitle_1" => "As code enthusiasts, we create modern technological solutions that inspire and simplify the daily lives of our clients.",
-            "subtitle_2" => "We ensure technical excellence, ease of use, and effectiveness in operation."
+            "subtitle_2" => "We ensure technical excellence, ease of use, and effectiveness in operation.",
         ],
         "section_3" => [
             "title_1" => "Values",
@@ -75,34 +75,34 @@ return [
             "content_3" => "Innovation: ",
             "content_4" => "We operate in the areas of big data processing, artificial intelligence, and optimization, developing modern technologies and promoting innovations.",
             "content_5" => "Technical excellence: ",
-            "content_6" => "We create software that works efficiently and reliably, is safe and easy to use. We hone our skills to create products that stand out in the market."
+            "content_6" => "We create software that works efficiently and reliably, is safe and easy to use. We hone our skills to create products that stand out in the market.",
         ],
         "section_4" => [
             "badge" => "after work",
             "title_1" => "Blumilk after hours",
             "subtitle_1" => "Our company promotes ",
             "subtitle_2" => "work-life balance, ",
-            "subtitle_3" => "giving employees the opportunity to develop outside of the professional sphere as well. After hours, our team builds bonds through shared passions such as board games, mountain trips, basketball tournaments, and regular outdoor meetings."
-        ]
+            "subtitle_3" => "giving employees the opportunity to develop outside of the professional sphere as well. After hours, our team builds bonds through shared passions such as board games, mountain trips, basketball tournaments, and regular outdoor meetings.",
+        ],
     ],
     "case_studies" => [
         "section_1" => [
             "title_1" => "Our ",
             "title_2" => "projects",
             "subtitle_1" => "See what we worked on for our clients.",
-            "subtitle_2" => "Each case study contains a detailed description of the project implementation."
+            "subtitle_2" => "Each case study contains a detailed description of the project implementation.",
         ],
         "section_2" => [
             "title_1" => "Interested in cooperation?",
-            "subtitle_1" => "Let us turn your vision into reality. Do you have an idea for a web/mobile application, need digital transformation, or plan an R&D project? Do it with us!"
-        ]
+            "subtitle_1" => "Let us turn your vision into reality. Do you have an idea for a web/mobile application, need digital transformation, or plan an R&D project? Do it with us!",
+        ],
     ],
     "case_study" => [
         "section_2" => [
             "title_1" => "Bring ",
             "title_2" => "your idea ",
             "title_3" => "to life with us",
-            "subtitle_1" => "We will help you turn your vision into a working solution. Do you have an idea for a web/mobile application, need digital transformation, or plan an R&D project? Do it with us!"
-        ]
-    ]
+            "subtitle_1" => "We will help you turn your vision into a working solution. Do you have an idea for a web/mobile application, need digital transformation, or plan an R&D project? Do it with us!",
+        ],
+    ],
 ];
