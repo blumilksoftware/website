fcf8db02fca60d4b48a58e1ab2d0b1173f5204b0
brzozka <brzozka108@gmail.com>
2024-06-12 14:55:49 +0200

    Example eng translation

---
+++ lang/en/contact.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    "title" => "Write",
+    "title2" => "to Us!",
+    "company" => "Blumilk Sp. z o.o.",
+    "subtitle" => "Are you interested enough to want to discuss your project with us, exchange ideas, become our business partner, or work with us? We would love to talk with you!",
+    "form.email" => "E-mail",
+    "form.title" => "Topic",
+    "form.message" => "Message",
+    "location" => "ul. Najświętszej Marii Panny 5f/5",
+    "locationPostalCode" => "59-220 Legnica",
+    "phone" => "+48 780 142 367",
+    "email" => "office@blumilk.pl",
+    "infoNIP" => "NIP 6912551135",
+    "infoKRS" => "KRS 0000866755",
+    "infoREGON" => "REGON 38737620",
+];
