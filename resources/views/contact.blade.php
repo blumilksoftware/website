ff8edd8732ff848d22f0d58e71646e12ddfedf24
brzozka <brzozka108@gmail.com>
2024-06-14 11:30:03 +0200

    Final styling adjustments

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -31,8 +31,8 @@
         </div>
     </div>
     <div
-        class="grid xl:grid-cols-2 justify-center  items-center  text-black bg-gray-light xl:bg-gradient-to-r from-gray-light from-30% to-white to-90%   ">
-        <section class=" md:flex flex-col   px-[10%] xl:px-[0%] xl:pl-[40%] pt-16 pb-7 z-20">
+        class="grid xl:grid-cols-2 justify-center  items-center  text-black bg-gray-light xl:bg-gradient-to-r from-gray-light from-30% to-white to-90%   xl:pb-8 ">
+        <section class=" md:flex flex-col   px-[10%] xl:px-[0%] xl:pl-[40%] pt-16 pb-4 z-20">
             <h4 class="text-3xl sm:text-4xl lg:text-5xl text-center xl:text-left font-semibold text-black "><span
                     class="text-website-normal">{{ __('contact.title') }}</span> {{ __('contact.title2') }}</h4>
             <p class="text-md xl:text-lg leading-8 text-black text-center xl:text-left py-6">{{ __('contact.subtitle') }}
@@ -45,7 +45,7 @@
                                 class="block text-xs font-semibold leading-6 text-gray-700">{{ __('contact.form.email') }}</label>
                             <div class="mt-2.5">
                                 <input type="text" name="e-mail" id="e-mail" autocomplete="given-e-mail"
-                                    class="block w-full  h-12 rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                                    class="block w-full xl:w-[90%]  h-12 rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
                             </div>
                         </div>
                         <div>
@@ -53,7 +53,7 @@
                                 class="block text-xs font-semibold leading-6 text-gray-700">{{ __('contact.form.title') }}</label>
                             <div class="mt-2.5">
                                 <input type="text" name="topic" id="topic" autocomplete="topic"
-                                    class="block w-full h-12 rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                                    class="block w-full xl:w-[90%]  h-12 rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
                             </div>
                         </div>
                         <div>
@@ -61,26 +61,26 @@
                                 class="block text-xs font-semibold leading-6 text-gray-700">{{ __('contact.form.message') }}</label>
                             <div class="mt-2.5">
                                 <textarea id="message" name="message" rows="4"
-                                    class="block w-full rounded-md border-0 px-3.5 py-2 min-h-12 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></textarea>
+                                    class="block w-full xl:w-[90%] rounded-md border-0 px-3.5 py-2 min-h-12 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></textarea>
                             </div>
                         </div>
                     </div>
-                    <div class="flex items-center mt-6">
+                    <div class="flex items-center mt-2">
                         <input type="checkbox" name="policyBox" id="policyBox" class="w-3 h-3">
                         <p class="text-xs font-normal text-gray-600 ml-2 pt-4">Akceptuję regulamin oraz <a href=""
                                 class="underline font-bold">politykę prywatności</a> i tym samym wyrażam zgodę na
                             przetwarzanie przez Administratora moich danych osobowych <a href=""
                                 class="font-bold">Więcej</a></p>
                     </div>
-                    <div class="mt-6 flex">
+                    <div class="mt-3 flex">
                         <x-primary-button type="submit" href=""
                             class=" w-full lg:w-[45%] 2xl:w-[32%] text-xl mx-0 bg-website-dark">{{ __('buttons.send') }}</x-primary-button>
                     </div>
                 </form>
             </div>
         </section>
-        <section class="flex flex-col h-full xl:pt-28 text-black px-[10%]  bg-white xl:bg-transparent pb-10 ">
-            <div class="xl:max-w-[460px] ">
+        <section class="flex flex-col h-full  text-black px-[10%]  bg-white xl:bg-transparent pb-4 ">
+            <div class="xl:max-w-[460px] h-full xl:pt-32 ">
                 <div
                     class="hidden absolute xl:flex xl:w-[455px] xl:h-[451px] -translate-y-[30px] translate-x-[25px] rotate-[4deg] bg-gray-light rounded-3xl ">
                 </div>
@@ -121,7 +121,7 @@
                     </div>
                 </div>
                 <div
-                    class=" hidden xl:flex justify-center xl:justify-end space-x-6 md:order-2 pt-8 items-center xl:mt-8 xl:pr-2">
+                    class=" hidden xl:flex  xl:justify-end space-x-6 md:order-2 items-center xl:mt-32 xl:pr-2 ">
                     <a href="https://clutch.co/profile/blumilk-0" class="text-gray-700 align-center fill-current"
                         target="_blank">
                         <span class="sr-only">Clutch</span>
