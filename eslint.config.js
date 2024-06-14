7ca91b4b6fab40af3cc81d441d64fb7c3f97118e
brzozka <brzozka108@gmail.com>
2024-06-14 10:33:17 +0200

    Revert eslint config changes

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,5 @@
-import blumilkDefault from "@blumilksoftware/eslint-config";
-import htmlPlugin from "eslint-plugin-html";
+import blumilkDefault from '@blumilksoftware/eslint-config'
 
 export default [
-  ...blumilkDefault,
-  {
-    files: [ "**/*.blade.php"],
-    settings: {
-      "html/html-extensions": [".html", ".blade.php"],
-    },
-    plugins: {
-      html: htmlPlugin, 
-    },
-    rules: {
-      "tailwindcss/no-custom-classname": "off",
-    },
-    languageOptions: {
-      globals: {
-        mapboxgl: "readonly",
-      },
-    },
-  },
-];
+    ...blumilkDefault,
+]
