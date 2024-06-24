b8279cf0c18b20863c576201c9fa624d04b85a28
Aleksandra Kozubal <aleksandra.kozubal@studenci.collegiumwitelona.pl>
2024-06-24 09:22:35 +0200

    lint

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -123,14 +123,12 @@
                 </div>
                 <div class="hidden 2xl:flex basis-5/12 m-auto md:delay-700 relative -right-5">
                     <div class="rounded-xl bg-gray-dark aspect-[2/1] max-h-[500px] rotate-6 relative -top-5">
-                        <div
-                            class="relative flex flex-col justify-between aspect-[2/1] max-h-[500px] z-10 right-6 top-10 rounded-2xl shadow-sm bg-white px-6 py-10 -rotate-6 gap-6">
+                        <div class="relative flex flex-col justify-between aspect-[2/1] max-h-[500px] z-10 right-6 top-10 rounded-2xl shadow-sm bg-white px-6 py-10 -rotate-6 gap-6">
                             <div class="px-4 font-semibold text-xl lg:text-2xl">
                                 {{__("content.home.section_3.title_2")}}
                             </div>
                             <div class="py-6 text-website-normal mx-4">
-                                <div
-                                    class="flex flex-col items-center place-content-center text-center bg-blue-light max-w-full px-6 py-4 gap-4 rounded-2xl">
+                                <div class="flex flex-col items-center place-content-center text-center bg-blue-light max-w-full px-6 py-4 gap-4 rounded-2xl">
                                     <img src="{{ asset('graphics/stars.svg') }}" class="max-h-8 shrink"
                                          alt="5 stars"/>
                                     <div class="leading-relaxed">
@@ -141,8 +139,7 @@
                             </div>
                             <div class="flex place-content-center px-8">
                                 <x-block-primary-button href="{{ route('contact') }}">
-                                    <span
-                                        class="font-semibold">{{ __("buttons.text_us_1") }}</span>{{ __("buttons.text_us_2") }}
+                                    <span class="font-semibold">{{ __("buttons.text_us_1") }}</span>{{ __("buttons.text_us_2") }}
                                 </x-block-primary-button>
                             </div>
                         </div>
@@ -198,10 +195,8 @@
                     <x-bubble size="20"/>
                 </span>
             </div>
-            <div
-                class="flex rounded-2xl xl:bg-gray-light -rotate-3 basis-8/12 relative xl:shadow-md size-full xl:left-[10%]">
-                <div
-                    class="absolute hidden xl:block rotate-3 z-20 rounded-2xl h-52 w-80 -left-[10%] bottom-[10%] bg-[url('graphics/code-snippet.png')] bg-cover">
+            <div class="flex rounded-2xl xl:bg-gray-light -rotate-3 basis-8/12 relative xl:shadow-md size-full xl:left-[10%]">
+                <div class="absolute hidden xl:block rotate-3 z-20 rounded-2xl h-52 w-80 -left-[10%] bottom-[10%] bg-[url('graphics/code-snippet.png')] bg-cover">
                 </div>
                 <img src="{{ asset('graphics/vita.png') }}"
                      class="w-full relative flex xl:left-[5%] overflow-hidden rounded-2xl bg-white shadow-md rotate-3"
