8dfe2990efb4448ec83d0dff5f6a5a3c4c696f13
EwelinaSkrzypacz <ewelina.skrzypacz@blumilk.pl>
2024-06-27 10:21:20 +0200

    - fix

--- database/seeders/UsersSeeder.php
+++ database/seeders/UsersSeeder.php
@@ -7,22 +7,35 @@ namespace Database\Seeders;
 use Blumilk\Website\Enums\Role;
 use Blumilk\Website\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class UsersSeeder extends Seeder
 {
+    protected const string DEFAULT_PASSWORD = "password";
+
     public function run(): void
     {
-        if (User::query()->count() > 0) {
-            return;
-        }
+        User::firstOrCreate(
+            ["email" => "admin@example.com"],
+            ["role" => Role::Admin,
+                "name" => "Administrator",
+                "email_verified_at" => now(),
+                "password" => Hash::make(static::DEFAULT_PASSWORD),
+                "remember_token" => Str::random(10),
+                "active" => true,
+            ],
+        );
 
-        User::factory()->create([
-            "email" => "admin@example.com",
-            "role" => Role::Admin,
-        ]);
-        User::factory()->create([
-            "email" => "moderator@example.com",
-            "role" => Role::Moderator,
-        ]);
+        User::firstOrCreate(
+            ["email" => "moderator@example.com"],
+            ["role" => Role::Moderator,
+                "name" => "Moderator",
+                "email_verified_at" => now(),
+                "password" => Hash::make(static::DEFAULT_PASSWORD),
+                "remember_token" => Str::random(10),
+                "active" => true,
+            ],
+        );
     }
 }
