8b0f1f10f1e41b54f7b7ff97609116aa0db4446c
Aleksandra Kozubal <aleksandra.kozubal@studenci.collegiumwitelona.pl>
2024-07-01 13:47:28 +0200

    move regex to const

--- app/Http/Requests/StoreRequest.php
+++ app/Http/Requests/StoreRequest.php
@@ -8,10 +8,12 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class StoreRequest extends FormRequest
 {
+    private const string EMAIL_PATTERN = "regex:/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/";
+
     public function rules(): array
     {
         return [
-            "email" => ["required", "email", "string", "max:255", "regex:/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/"],
+            "email" => ["required", "email", "string", "max:255", self::EMAIL_PATTERN],
             "topic" => ["required", "string", "max:255"],
             "message" => ["required", "string", "max:65000"],
             "consents" => ["accepted"],
