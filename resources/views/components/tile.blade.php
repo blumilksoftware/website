8b8324f799d03376dc5d999f5f2e84377c65a4b4
Aleksandra Kozubal <aleksandra.kozubal@studenci.collegiumwitelona.pl>
2024-07-28 14:20:12 +0200

    sections about.blade.php

--- resources/views/components/tile.blade.php
+++ resources/views/components/tile.blade.php
@@ -1,11 +1,17 @@
-<div class="flex flex-col items-center 2xl:place-items-start 2xl:flex-row bg-blue-light rounded-xl lg:text-lg p-6 gap-4 w-full h-full shrink">
-    <img src="{{ $image }}" class="size-20 2xl:m-4" alt="{{ $alt }}">
-    <div class="2xl:mt-2 text-center 2xl:text-left">
-        <span class="font-semibold text-md block pb-2">
-            {!! $title !!}
-        </span>
-        <span class="font-normal text-gray-400 text-sm leading-3">
-            {{ $description }}
-        </span>
+<div class="{{$attributes->get('class')}}">
+    <div class="flex flex-col place-items-center 2xl:place-items-start 2xl:flex-row bg-gray-extraLight rounded-xl leading-relaxed lg:text-lg p-6 gap-4 w-full h-full shrink">
+        <div class="bg-white shadow-xl rounded-full p-4 size-14 aspect-square">
+            {{ $slot }}
+        </div>
+
+        <div class="2xl:mt-2 text-center 2xl:text-left">
+            <span class="font-semibold text-md block pb-2">
+                {!! $title !!}
+            </span>
+            <span class="font-normal text-gray-400 text-sm leading-3">
+                {{ $description }}
+            </span>
+        </div>
     </div>
+
 </div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -148,24 +148,6 @@
                 </div>
             </div>
         </section>
-        <section class="relative mx-[10%] 2xl:max-w-[1500px] 2xl:mx-auto">
-            <div class="w-full py-12 sm:py-32">
-                <div class="relative isolate overflow-hidden bg-blue-normal py-24 text-center shadow-2xl px-8  rounded-3xl">
-                    <img src="{{ asset('graphics/blobs/blob_1.svg') }}" class="absolute -left-44 2xl:-left-16 -bottom-44 xl:-bottom-32" alt="blob"/>
-                    <img src="{{ asset('graphics/blobs/blob_2.svg') }}" class="absolute -z-20 -left-32 -top-24 xl:-top-16 2xl:-top-8 3xl:top-0" alt="blob"/>
-                    <img src="{{ asset('graphics/blobs/blob_3.svg') }}" class="absolute -z-20 -right-16 2xl:-right-8 3xl:right-0 top-0" alt="blob"/>
-                    <img src="{{ asset('graphics/blobs/blob_4.svg') }}" class="absolute -z-20 -bottom-64 2xl:-bottom-44 3xl:-bottom-32 -right-44 xl:right-6 2xl:right-16" alt="blob"/>
-                    <h2 class="mx-auto max-w-5xl text-3xl font-bold tracking-tight text-white sm:text-5xl">{{ __("content.home.section_6.title_1") }}</h2>
-                    <p class="mx-auto max-w-7xl mt-6 text-lg leading-8 text-gray-300">{{ __("content.home.section_6.subtitle_1") }}
-                        <span class="font-bold text-white block">{{ __("content.home.section_6.subtitle_2") }}</span>
-                    </p>
-                    <div class="mt-10 flex items-center justify-center gap-x-6">
-                        <x-primary-button href="{{ route('contact') }}" class="px-16 bg-blue-dark opacity-80">
-                            {{ __("buttons.text_us") }}
-                        </x-primary-button>
-                    </div>
-                </div>
-            </div>
-        </section>
+        <x-text-us-section/>
     </section>
 @endsection
