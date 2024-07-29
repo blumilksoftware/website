612490d5225729f66dfebe35466b1317045e3aaa
krzysztofrewak <krzysztof.rewak@blumilk.pl>
2024-07-29 13:13:20 +0200

    #304 - i18n problems

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,8 +4,21 @@ declare(strict_types=1);
 
 namespace Blumilk\Website\Providers;
 
+use Illuminate\Http\Request;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
+    public function boot(): void
+    {
+        /** @var Request $request */
+        $request = app("request");
+
+        dump($request);
+
+        if($request->getRequestUri() === "/" && str_starts_with($request->header("Accept-Language"), "pl")) {
+            $request->setLocale("pl");
+            app()->setLocale("pl");
+        }
+    }
 }
--- tests/Feature/LocalisedRoutingFallbackTest.php
+++ tests/Feature/LocalisedRoutingFallbackTest.php
@@ -10,17 +10,29 @@ class LocalisedRoutingFallbackTest extends TestCase
 {
     public function testFallbackLocaleForDefaultRedirect(): void
     {
+        $response = $this->get("/");
+        $response->assertRedirect("/en");
+    }
+
+    public function testFallbackLocaleForPolishRedirect(): void
+    {
+        $response = $this->get("/", ["Accept-Language" => "pl"]);
+        $response->assertRedirect("/pl");
+    }
+
+    public function testFallbackLocaleForContactPage(): void
+    {
         $response = $this->get("/contact");
         $response->assertRedirect("/en/contact");
     }
 
-    public function testFallbackLocaleForPolishRedirect(): void
+    public function testFallbackLocaleForPolishContactPage(): void
     {
         $response = $this->get("/contact", ["Accept-Language" => "pl"]);
         $response->assertRedirect("/en/contact");
     }
 
-    public function testFallbackLocaleForNotSupportedLanguageRedirect(): void
+    public function testFallbackLocaleForNotSupportedLanguageContactPage(): void
     {
         $response = $this->get("/contact", ["Accept-Language" => "kz"]);
         $response->assertRedirect("/en/contact");
