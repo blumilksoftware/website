761dac8570fc39fea0543bd07aa1c63bd91df1fa
Krzysztof Rewak <krzysztof.rewak@blumilk.pl>
2024-08-05 14:39:57 +0200

    #304 - language detection (#321)
    * #304 - i18n problems
    
    * #304 - i18n problems
    
    * #304 - i18n problems partially solved

--- tests/Feature/LocalisedRoutingFallbackTest.php
+++ tests/Feature/LocalisedRoutingFallbackTest.php
@@ -10,17 +10,33 @@ class LocalisedRoutingFallbackTest extends TestCase
 {
     public function testFallbackLocaleForDefaultRedirect(): void
     {
-        $response = $this->get("/contact");
-        $response->assertRedirect("/en/contact");
+        $response = $this->get("/");
+        $response->assertRedirect("/en");
     }
 
     public function testFallbackLocaleForPolishRedirect(): void
     {
-        $response = $this->get("/contact", ["Accept-Language" => "pl"]);
+        $this->app->setLocale("pl");
+
+        $response = $this->get("/", ["Accept-Language" => "pl"]);
+        $response->assertRedirect("/pl");
+    }
+
+    public function testFallbackLocaleForContactPage(): void
+    {
+        $response = $this->get("/contact");
         $response->assertRedirect("/en/contact");
     }
 
-    public function testFallbackLocaleForNotSupportedLanguageRedirect(): void
+    public function testFallbackLocaleForPolishContactPage(): void
+    {
+        $this->app->setLocale("pl");
+
+        $response = $this->get("/kontakt", ["Accept-Language" => "pl"]);
+        $response->assertRedirect("/pl/kontakt");
+    }
+
+    public function testFallbackLocaleForNotSupportedLanguageContactPage(): void
     {
         $response = $this->get("/contact", ["Accept-Language" => "kz"]);
         $response->assertRedirect("/en/contact");
