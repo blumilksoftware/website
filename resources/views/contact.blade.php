15560356370a8c8f72b596079b93e296bb0c9e1a
Aleksandra Kozubal <aleksandra.kozubal@studenci.collegiumwitelona.pl>
2024-08-06 15:04:15 +0200

    rwd

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -8,15 +8,78 @@
         <x-toast type="success" position="top-right">{{ Session::get('success') }}</x-toast>
     @endif
 
-    <div class="mx-[10%] md:mx-[15%] 2xl:max-w-7xl 2xl:mx-auto flex flex-col xl:flex-row justify-center text-black xl:pb-8 mt-12">
-        <div class="basis-1/2 flex flex-col gap-y-10">
-            <div>
-                <h4 class="text-2xl sm:text-3xl lg:text-4xl text-center xl:text-left font-bold text-black pb-4">
-                    {{ __('content.contact.title_1') }}</h4>
-                <p class="text-md leading-relaxed text-gray-500 text-center xl:text-left">
-                    {{ __('content.contact.subtitle') }}
-                </p>
-            </div>
+    <div class="mx-[10%] md:mx-[15%] 2xl:max-w-7xl 2xl:mx-auto grid xl:grid-cols-2 xl:grid-rows-4 grid-flow-row-dense content-center text-black xl:pb-8 mt-12 items-start">
+        <div class="h-min">
+            <h4 class="text-2xl sm:text-3xl lg:text-4xl text-center xl:text-left font-bold text-black pb-4">
+                {{ __('content.contact.title_1') }}</h4>
+            <p class="text-md leading-relaxed text-gray-500 text-center xl:text-left">
+                {{ __('content.contact.subtitle') }}
+            </p>
+        </div>
+        <div class="row-span-4 py-12 xl:p-0">
+            <form action="{{ route('contact.create') }}" method="POST" class="flex flex-col gap-y-4 justify-between h-full xl:ml-[15%] xl:bg-[#F4F8FD] md:px-16 md:py-10 rounded-xl">
+                @csrf
+                <div class="grid gap-y-3">
+                    <div>
+                        <label for="e-mail"
+                               class="block text-sm font-normal leading-6 text-gray-700 ml-2">{{ __('content.contact.form.email') }}</label>
+                        <div class="mt-2.5 ">
+                            <input name="email" id="email" autocomplete="given-e-mail"
+                                   value="{{ old('email') }}"
+                                   class="block w-full h-12 rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 xl:bg-[#fafcfe] placeholder:text-gray-400 focus:ring-1 focus:ring-gray-400 sm:text-sm sm:leading-6">
+                            @error('email')
+                            <p class="mt-2 text-xs text-red-600">{{ $message }}</p>
+                            @enderror
+                        </div>
+                    </div>
+                    <div>
+                        <label for="topic"
+                               class="block text-sm font-normal leading-6 text-gray-700 ml-2">{{ __('content.contact.form.title') }}</label>
+                        <div class="mt-2.5">
+                            <input type="text" name="topic" id="topic" autocomplete="topic"
+                                   value="{{ old('topic') }}"
+                                   class="block w-full h-12 rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 xl:bg-[#fafcfe] placeholder:text-gray-400 focus:ring-1 focus:ring-gray-400 sm:text-sm sm:leading-6">
+                            @error('topic')
+                            <p class="mt-2 text-xs text-red-600">{{ $message }}</p>
+                            @enderror
+                        </div>
+                    </div>
+                    <div>
+                        <label for="message"
+                               class="block text-sm font-normal leading-6 text-gray-700 ml-2">{{ __('content.contact.form.message') }}</label>
+                        <div class="mt-2.5">
+                                <textarea id="message" name="message" rows="10" autocomplete="message"
+                                          class="block w-full rounded-md border-0 px-3.5 py-2 min-h-12 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 xl:bg-[#fafcfe] placeholder:text-gray-400 focus:ring-1 focus:ring-gray-400 sm:text-sm sm:leading-6">{{ old('message') }}</textarea>
+                            @error('message')
+                            <p class="mt-2 text-xs text-red-600">{{ $message }}</p>
+                            @enderror
+                        </div>
+                    </div>
+                </div>
+                <div class="flex flex-col items-left mt-2">
+                    <label for="consents" class="flex items-center">
+                        <input type="checkbox" name="consents" id="consents"
+                               class="w-4 h-4 rounded-sm xl:bg-[#fafcfe] border-gray-300 focus:ring-0 checked:text-website-light">
+                        <span class="text-xs font-normal text-gray-500 ml-2 pt-4">
+                                    {{ __('content.contact.policy.part_1') }}
+                                    <a href="{{ route('privacy-policy') }}" target="_blank"
+                                       class="underline font-bold">{{ __('content.contact.policy.part_2') }}</a>
+                                    {{ __('content.contact.policy.part_3') }}
+                                </span>
+                    </label>
+                    @error('consents')
+                    <p class="mt-2 text-xs text-red-600">{{ $message }}</p>
+                    @enderror
+                </div>
+                <div class="mt-3 flex">
+                    <button type="submit"
+                            class="w-full text-xl mx-0 bg-website-normal m-2 p-3 sm:text-lg h-min text-center font-normal text-white shadow-sm rounded-xl transform transition hover:scale-110 flex justify-center items-center">
+                        {{ __('buttons.send_message') }}
+                    </button>
+                </div>
+            </form>
+        </div>
+        <div class="h-full row-span-3">
             <div id="map" class="h-[40vh] xl:h-[20vh] w-full rounded-xl"></div>
             <div>
                 <h1 class="text-2xl font-semibold text-black mb-4">{{ __('content.contact.company') }}</h1>
@@ -41,70 +104,6 @@
                     </div>
                 </div>
             </div>
-        </div>
-        <div class="basis-1/2">
-            <form action="{{ route('contact.create') }}" method="POST" class="flex flex-col gap-y-4 justify-between h-full ml-36 bg-[#F4F8FD] px-16 py-10 rounded-xl">
-                @csrf
-                <div class="grid gap-y-3">
-                    <div>
-                        <label for="e-mail"
-                               class="block text-sm font-normal leading-6 text-gray-700 ml-2">{{ __('content.contact.form.email') }}</label>
-                        <div class="mt-2.5 ">
-                            <input name="email" id="email" autocomplete="given-e-mail"
-                                   value="{{ old('email') }}"
-                                   class="block w-full h-12 rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 bg-[#fafcfe] placeholder:text-gray-400 focus:ring-1 focus:ring-gray-400 sm:text-sm sm:leading-6">
-                            @error('email')
-                            <p class="mt-2 text-xs text-red-600">{{ $message }}</p>
-                            @enderror
-                        </div>
-                    </div>
-                    <div>
-                        <label for="topic"
-                               class="block text-sm font-normal leading-6 text-gray-700 ml-2">{{ __('content.contact.form.title') }}</label>
-                        <div class="mt-2.5">
-                            <input type="text" name="topic" id="topic" autocomplete="topic"
-                                   value="{{ old('topic') }}"
-                                   class="block w-full h-12 rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 bg-[#fafcfe] placeholder:text-gray-400 focus:ring-1 focus:ring-gray-400 sm:text-sm sm:leading-6">
-                            @error('topic')
-                            <p class="mt-2 text-xs text-red-600">{{ $message }}</p>
-                            @enderror
-                        </div>
-                    </div>
-                    <div>
-                        <label for="message"
-                               class="block text-sm font-normal leading-6 text-gray-700 ml-2">{{ __('content.contact.form.message') }}</label>
-                        <div class="mt-2.5">
-                                <textarea id="message" name="message" rows="10" autocomplete="message"
-                                          class="block w-full rounded-md border-0 px-3.5 py-2 min-h-12 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 bg-[#fafcfe] placeholder:text-gray-400 focus:ring-1 focus:ring-gray-400 sm:text-sm sm:leading-6">{{ old('message') }}</textarea>
-                            @error('message')
-                            <p class="mt-2 text-xs text-red-600">{{ $message }}</p>
-                            @enderror
-                        </div>
-                    </div>
-                </div>
-                <div class="flex flex-col items-left mt-2 xl:w-[90%]">
-                    <label for="consents" class="flex items-center">
-                        <input type="checkbox" name="consents" id="consents"
-                               class="w-4 h-4 rounded-sm bg-[#fafcfe] border-gray-300 focus:ring-0 checked:text-website-light">
-                        <span class="text-xs font-normal text-gray-500 ml-2 pt-4">
-                                    {{ __('content.contact.policy.part_1') }}
-                                    <a href="{{ route('privacy-policy') }}" target="_blank"
-                                       class="underline font-bold">{{ __('content.contact.policy.part_2') }}</a>
-                                    {{ __('content.contact.policy.part_3') }}
-                                </span>
-                    </label>
-                    @error('consents')
-                    <p class="mt-2 text-xs text-red-600">{{ $message }}</p>
-                    @enderror
-                </div>
-
-                <div class="mt-3 flex">
-                    <button type="submit"
-                            class="w-full text-xl mx-0 bg-website-normal m-2 p-3 sm:text-lg h-min text-center font-normal text-white shadow-sm rounded-xl transform transition hover:scale-110 flex justify-center items-center">
-                        {{ __('buttons.send_message') }}
-                    </button>
-                </div>
-            </form>
         </div>
     </div>
 
