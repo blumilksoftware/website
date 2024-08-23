492e44db1a52f47d8e1a438ef43d858f41d517da
Aleksandra Kozubal <104600942+AleksandraKozubal@users.noreply.github.com>
2024-08-23 13:43:56 +0200

    #301 - career page mobile (#328)
    * mobile view, color change in nav, remove unnecessary scripts
    
    * shadows position
    
    * cr changes

--- resources/views/career.blade.php
+++ resources/views/career.blade.php
@@ -4,7 +4,7 @@
 @section("description", __("meta.career.description"))
 
 @section("content")
-    <section class="flex flex-col-reverse xl:flex-row pt-28 mx-[10%] lg:mx-[15%] 2xl:max-w-7xl 2xl:mx-auto">
+    <section class="flex flex-col-reverse xl:flex-row pt-8 md:pt-28 mx-[10%] lg:mx-[15%] 2xl:max-w-7xl 2xl:mx-auto">
         <div class="flex basis-2/5 relative flex-col self-center gap-5">
             <div class="text-3xl md:text-4xl lg:text-5xl text-center xl:justify-start xl:text-start">
                 <h2 class="font-semibold text-gray-900 leading-snug">
@@ -13,11 +13,11 @@
                 </h2>
             </div>
             <div class="flex flex-col items-center xl:items-start">
-                <p class="text-lg font-normal text-center xl:text-start pb-12">
-                    <span class="leading-relaxed">{{ __("content.career.section_1.subtitle_1") }}</span>
+                <p class="text-sm md:text-lg font-normal text-center xl:text-start pb-12 md:pb-12">
+                    <span class="leading-loose md:leading-relaxed">{{ __("content.career.section_1.subtitle_1") }}</span>
                 </p>
-                <p class="text-lg font-normal text-center xl:text-start">
-                    <span class="leading-relaxed block">{{ __("content.career.section_1.subtitle_2") }}</span>
+                <p class="text-sm md:text-lg font-normal text-center xl:text-start">
+                    <span class="leading-loose md:leading-relaxed md:block">{{ __("content.career.section_1.subtitle_2") }}</span>
                     <span class="leading-relaxed font-semibold">{{ __("content.career.section_1.subtitle_3") }}</span>
                 </p>
                 <div class="hidden sm:block my-6">
@@ -25,70 +25,122 @@
                 </div>
             </div>
         </div>
-        <div class="flex basis-3/5 relative self-center pb-24 xl:pb-0">
+        <div class="flex basis-3/5 relative self-center pb-8 md:pb-24 xl:pb-0">
             <img src="{{ asset('graphics/career.svg') }}" class="h-auto w-full aspect-auto object-center shrink-0"
                  alt="placeholder"/>
         </div>
     </section>
     <section class="relative">
-        <div class="mx-[10%] lg:mx-[15%] 2xl:max-w-7xl 2xl:mx-auto space-y-10 pt-28 pb-36">
-            <div class="pb-14 text-center">
+        <div class="mx-[10%] lg:mx-[15%] 2xl:max-w-7xl 2xl:mx-auto md:space-y-10 pt-28 pb-20 md:pb-36">
+            <div class="pb-10 md:pb-14 text-center">
                 <h2 class="text-3xl md:text-4xl lg:text-5xl font-semibold pb-8">{{ __("content.career.section_2.title_1") }}</h2>
-                <span class="font-normal leading-relaxed text-lg relative">{{ __("content.career.section_2.subtitle_1") }}</span>
+                <span class="font-normal leading-loose md:leading-relaxed text-sm md:text-lg relative">{{ __("content.career.section_2.subtitle_1") }}</span>
             </div>
-            <div class="grid md:grid-cols-2 xl:grid-cols-3 gap-4 2xl:gap-12 place-items-center xl:place-items-stretch">
-                <x-tile title="{{ __('content.career.section_2.benefit_1') }}" class="">
-                    <x-icons.clock accent="text-website-normal"/>
-                </x-tile>
-                <x-tile title="{{ __('content.career.section_2.benefit_2') }}">
-                    <x-icons.school accent="text-website-normal"/>
-                </x-tile>
-                <x-tile title="{{ __('content.career.section_2.benefit_3') }}">
-                    <x-icons.stethoscope accent="text-website-normal"/>
-                </x-tile>
-                <x-tile title="{{ __('content.career.section_2.benefit_4') }}">
-                    <x-icons.treadmill accent="text-website-normal"/>
-                </x-tile>
-                <x-tile title="{{ __('content.career.section_2.benefit_5') }}">
-                    <x-icons.lemon accent="text-website-normal"/>
-                </x-tile>
-                <x-tile title="{{ __('content.career.section_2.benefit_6') }}">
-                    <x-icons.smile accent="text-website-normal"/>
-                </x-tile>
+            <div class="grid md:grid-cols-2 xl:grid-cols-3 gap-4 2xl:gap-12 xl:place-items-stretch">
+                <div class="bg-gray-extraLight rounded-xl md:px-6">
+                    <div class="flex flex-row leading-relaxed lg:text-lg py-6 w-fit h-full">
+                        <div class="self-start bg-white shadow-md rounded-full mr-5 ml-10 p-4 size-16 aspect-square">
+                            <x-icons.clock accent="text-website-normal"/>
+                        </div>
+                        <div class="self-center 2xl:mt-2 text-left">
+                            <span class="font-semibold text-lg block pr-10 leading-6">
+                                {{ __('content.career.section_2.benefit_1') }}
+                            </span>
+                        </div>
+                    </div>
+                </div>
+                <div class="bg-gray-extraLight rounded-xl md:px-6">
+                    <div class="flex flex-row leading-relaxed lg:text-lg py-6 w-fit h-full">
+                        <div class="self-start bg-white shadow-md rounded-full mr-5 ml-10 p-4 size-16 aspect-square">
+                            <x-icons.school accent="text-website-normal"/>
+                        </div>
+                        <div class="self-center 2xl:mt-2 text-left">
+                            <span class="font-semibold text-lg block pr-3 2xl:pr-10 leading-6">
+                                {{ __('content.career.section_2.benefit_2') }}
+                            </span>
+                        </div>
+                    </div>
+                </div>
+                <div class="bg-gray-extraLight rounded-xl md:px-6">
+                    <div class="flex flex-row leading-relaxed lg:text-lg py-6 w-fit h-full">
+                        <div class="self-start bg-white shadow-md rounded-full mr-5 ml-10 p-4 size-16 aspect-square">
+                            <x-icons.stethoscope accent="text-website-normal"/>
+                        </div>
+                        <div class="self-center 2xl:mt-2 text-left">
+                            <span class="font-semibold text-lg block pr-6 md:pr-8 leading-6">
+                                {{ __('content.career.section_2.benefit_3') }}
+                            </span>
+                        </div>
+                    </div>
+                </div>
+                <div class="bg-gray-extraLight rounded-xl md:px-6">
+                    <div class="flex flex-row leading-relaxed lg:text-lg py-6 w-fit h-full">
+                        <div class="self-start bg-white shadow-md rounded-full mr-5 ml-10 p-4 size-16 aspect-square">
+                            <x-icons.treadmill accent="text-website-normal"/>
+                        </div>
+                        <div class="self-center 2xl:mt-2 text-left">
+                            <span class="font-semibold text-lg block pr-10 leading-6">
+                                {{ __('content.career.section_2.benefit_4') }}
+                            </span>
+                        </div>
+                    </div>
+                </div>
+                <div class="bg-gray-extraLight rounded-xl md:px-6">
+                    <div class="flex flex-row leading-relaxed lg:text-lg py-6 w-fit h-full">
+                        <div class="self-start bg-white shadow-md rounded-full mr-5 ml-10 p-4 size-16 aspect-square">
+                            <x-icons.lemon accent="text-website-normal"/>
+                        </div>
+                        <div class="self-center 2xl:mt-2 text-left">
+                            <span class="font-semibold text-lg block pr-24 leading-6">
+                                {{ __('content.career.section_2.benefit_5') }}
+                            </span>
+                        </div>
+                    </div>
+                </div>
+                <div class="bg-gray-extraLight rounded-xl md:px-6">
+                    <div class="flex flex-row leading-relaxed lg:text-lg py-6 w-fit h-full">
+                        <div class="self-start bg-white shadow-md rounded-full mr-5 ml-10 p-4 size-16 aspect-square">
+                            <x-icons.smile accent="text-website-normal"/>
+                        </div>
+                        <div class="self-center 2xl:mt-2 text-left">
+                            <span class="font-semibold text-lg block pr-10 leading-6">
+                                {{ __('content.career.section_2.benefit_6') }}
+                            </span>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </section>
-    <section class="flex flex-col-reverse mt-10 pb-5 xl:flex-row mx-[10%] lg:mx-[15%] 2xl:max-w-7xl 2xl:mx-auto gap-x-24">
-        <div class="self-center basis-3/5">
-            <img src="{{ asset('graphics/career_2.svg') }}" class="h-auto w-full object-center shrink-0"
-                 alt="{{ __("content.career.section_3.title_1") }}"/>
-        </div>
-        <div class="flex basis-2/5 relative flex-col justify-center pb-24 xl:pb-0">
-            <div class="text-center xl:justify-start xl:text-start xl:pb-10">
-                <h2 class="font-semibold text-gray-900 leading-snug text-3xl md:text-4xl lg:text-5xl mb-10">{{ __("content.career.section_4.title_1") }}</h2>
-                <p class="text-lg font-normal text-center xl:text-start">
-                    <span class="leading-relaxed">{{ __("content.career.section_4.subtitle_1") }}</span>
-                </p>
+    <section class="relative overflow-hidden">
+        <div class="flex flex-col md:mt-10 md:pb-5 xl:flex-row mx-[10%] lg:mx-[15%] 2xl:max-w-7xl 2xl:mx-auto gap-x-24">
+            <div class="self-center basis-3/5">
+                <img src="{{ asset('graphics/career_3.svg') }}" class="h-auto w-full object-center shrink-0 pb-10 md:pb-0"
+                     alt="{{ __("content.career.section_3.title_1") }}"/>
             </div>
-            <x-primary-button href="{{ route('career.software-engineer') }}" class="px-16 my-6 flex m-auto xl:m-0 xl:mr-auto xl:justify-start bg-website-normal">{{ __("buttons.apply_now") }}</x-primary-button>
+            <div class="flex basis-2/5 relative flex-col justify-center md:pb-24 xl:pb-0">
+                <div class="text-center xl:justify-start xl:text-start pb-5 xl:pb-10">
+                    <h2 class="font-semibold text-gray-900 leading-snug text-md md:text-4xl lg:text-5xl mb-4 md:mb-10">{{ __("content.career.section_4.title_1") }}</h2>
+                    <p class="text-xs md:text-lg font-normal text-center xl:text-start text-gray-400 leading-loose md:leading-relaxed mx-8 md:mx-0">{{ __("content.career.section_4.subtitle_1") }}</p>
+                </div>
+                <x-primary-button href="{{ route('career.software-engineer') }}" class="px-16 my-6 flex w-full m-auto xl:m-0 xl:mr-auto xl:justify-start bg-website-normal">{{ __("buttons.apply_now") }}</x-primary-button>
+            </div>
         </div>
-    </section>
-    <section class="relative flex xl:flex-row mx-[10%] lg:mx-[15%] 2xl:max-w-7xl 2xl:mx-auto pt-5 mb-16 xl:mb-28 gap-x-24">
-        <img src="{{ asset('graphics/shadow.svg') }}" alt="blob" class="absolute -z-10 -right-[60%] -bottom-44"/>
-        <img src="{{ asset('graphics/shadow.svg') }}" alt="blob" class="absolute -z-10 -left-[80%] -top-60"/>
+        <img src="{{ asset('graphics/shadow.svg') }}" alt="blob" class="hidden md:block absolute -z-10 -right-[50%] -bottom-44"/>
+        <img src="{{ asset('graphics/shadow.svg') }}" alt="blob" class="hidden md:block absolute -z-10 -left-[80%] top-40 object-cover"/>
 
-        <div class="flex xl:basis-2/5 flex-col justify-start xl:justify-center">
-            <div class="text-center xl:justify-start xl:text-start xl:pb-10">
-                <h2 class="font-semibold text-gray-900 leading-snug text-3xl md:text-4xl lg:text-5xl mb-10">{{ __("content.career.section_3.title_1") }}</h2>
-                <p class="text-lg font-normal text-center xl:text-start">
-                    <span class="leading-relaxed text-lg">{{ __("content.career.section_3.subtitle_1") }}</span>
-                </p>
+        <div class="mx-[10%] lg:mx-[15%] 2xl:max-w-7xl 2xl:mx-auto flex flex-col-reverse xl:flex-row gap-x-24 md:pt-5 md:mb-16 xl:mb-44">
+            <div class="flex xl:basis-2/5 flex-col justify-start xl:justify-center overflow-visible">
+                <div class="text-center xl:justify-start xl:text-start pb-5 xl:pb-10">
+                    <h2 class="font-semibold text-gray-900 leading-snug text-md md:text-4xl lg:text-5xl mb-4 md:mb-10">{{ __("content.career.section_3.title_1") }}</h2>
+                    <p class="text-xs md:text-lg font-normal text-center xl:text-start text-gray-400 leading-loose md:leading-relaxed mx-8 md:mx-0">{{ __("content.career.section_3.subtitle_1") }}</p>
+                </div>
+                <x-primary-button href="https://praktyki.blumilk.pl/" target="_blank" class="px-16 my-6 flex w-full m-auto xl:m-0 xl:mr-auto xl:justify-start bg-website-normal">{{ __("buttons.apply_internship") }}</x-primary-button>
             </div>
-            <x-primary-button href="https://praktyki.blumilk.pl/" target="_blank" class="px-16 my-6 flex m-auto xl:m-0 xl:mr-auto xl:justify-start bg-website-normal">{{ __("buttons.apply_internship") }}</x-primary-button>
-        </div>
-        <div class="hidden xl:flex relative self-center basis-3/5">
-            <img src="{{ asset('graphics/career_3.svg') }}" class="h-auto w-full object-center shrink-0"
-                 alt="{{ __("content.career.section_4.title_1") }}"/>
+            <div class="flex relative self-center basis-3/5">
+                <img src="{{ asset('graphics/career_2.svg') }}" class="h-auto w-full object-center shrink-0 pt-4 pb-10 md:pb-0"
+                     alt="{{ __("content.career.section_4.title_1") }}"/>
+            </div>
         </div>
     </section>
     <x-text-us-section/>
--- resources/views/software-engineer.blade.php
+++ resources/views/software-engineer.blade.php
@@ -4,7 +4,7 @@
 @section("description", __("meta.software-engineer.description"))
 
 @section("content")
-    <section class="flex flex-col-reverse xl:flex-row pt-28 pb-10 mx-[10%] lg:mx-[15%] 2xl:max-w-7xl 2xl:mx-auto">
+    <section class="flex flex-col-reverse xl:flex-row pt-8 md:pt-28 pb-6 md:pb-10 mx-[10%] lg:mx-[15%] 2xl:max-w-7xl 2xl:mx-auto">
         <div class="flex basis-2/5 relative flex-col self-center gap-5">
             <div class="text-3xl md:text-4xl lg:text-5xl text-center xl:justify-start xl:text-start">
                 <h2 class="font-semibold text-gray-900 leading-snug">
@@ -12,45 +12,45 @@
                     <span class="text-website-normal">{{ __("content.software_engineer.section_1.title_2") }}</span>
                 </h2>
             </div>
-            <div class="flex flex-col items-center xl:items-start text-lg font-normal text-center xl:text-start pb-12 leading-relaxed">
+            <div class="flex flex-col items-center xl:items-start text-sm md:text-lg font-normal text-center xl:text-start pb-12 leading-8 md:leading-relaxed">
                 {{ __("content.software_engineer.section_1.subtitle_1") }}
             </div>
         </div>
-        <div class="flex basis-3/5 relative self-center pb-24 xl:pb-0">
+        <div class="flex basis-3/5 relative self-center pb-8 md:pb-24 xl:pb-0">
             <img src="{{ asset('graphics/software-engineer.svg') }}" class="h-auto w-full aspect-auto object-center shrink-0"
                  alt="placeholder"/>
         </div>
     </section>
-    <section class="mx-[10%] lg:mx-[15%] 2xl:max-w-7xl 2xl:mx-auto pt-28 pb-8">
-        <div class="flex flex-col xl:flex-row gap-4 2xl:gap-8">
-            <div class="flex flex-col place-items-start xl:flex-row bg-gray-extraLight rounded-xl leading-relaxed  py-6 px-12 gap-2 size-auto">
-                <div class="bg-white shadow-md rounded-full size-16 aspect-square mt-4 mx-auto sm:mx-0 flex place-items-center justify-center">
+    <section class="mx-[10%] lg:mx-[15%] 2xl:max-w-7xl 2xl:mx-auto md:pt-28 pb-8">
+        <div class="flex flex-col lg:flex-row gap-4 2xl:gap-8">
+            <div class="flex flex-col place-items-start sm:flex-row bg-gray-extraLight rounded-xl leading-relaxed py-6 px-12 gap-4 size-auto md:justify-center">
+                <div class="bg-white shadow-md rounded-full size-16 aspect-square mt-4 xl:mx-auto sm:mx-0 flex place-items-center justify-center">
                     <x-icons.device-desktop/>
                 </div>
                 <div class="2xl:mt-2 text-left ml-4">
-                    <span class="font-semibold text-lg block pb-2">
+                    <span class="font-semibold text-sm md:text-lg block pb-2 -ml-4">
                         {{ __('content.software_engineer.section_2.stack.title_1') }}
                     </span>
-                    <ul class="list-square list-inside font-normal text-gray-400 text-sm leading-relaxed">
+                    <ul class="list-square font-normal text-gray-400 text-xs md:text-sm">
                         @for($stackListItem = 1; $stackListItem <= 9; $stackListItem++)
-                            <li class="text-md leading-relaxed">
+                            <li class="text-md leading-7 md:leading-relaxed">
                                 {{ __("content.software_engineer.section_2.stack.content_$stackListItem") }}
                             </li>
                         @endfor
                     </ul>
                 </div>
             </div>
-            <div class="flex flex-col place-items-start xl:flex-row bg-gray-extraLight rounded-xl leading-relaxed py-6 px-12 gap-2 size-auto">
-                <div class="bg-white shadow-md rounded-full size-16 aspect-square mt-4 mx-auto sm:mx-0 flex place-items-center justify-center">
+            <div class="flex flex-col place-items-start sm:flex-row bg-gray-extraLight rounded-xl leading-relaxed py-6 px-12 gap-4 size-auto md:justify-center">
+                <div class="bg-white shadow-md rounded-full size-16 aspect-square mt-4 xl:mx-auto sm:mx-0 flex place-items-center justify-center">
                     <x-icons.crown/>
                 </div>
                 <div class="2xl:mt-2 text-left ml-4">
-                    <span class="font-semibold text-lg block pb-2">
+                    <span class="font-semibold text-sm md:text-lg block pb-2 -ml-4">
                         {{ __('content.software_engineer.section_2.requirements.title_1') }}
                     </span>
-                    <ul class="list-square list-inside font-normal text-gray-400 text-sm leading-relaxed">
+                    <ul class="list-square font-normal text-gray-400 text-xs md:text-sm">
                         @for($stackListItem = 1; $stackListItem <= 9; $stackListItem++)
-                            <li class="text-md leading-relaxed">
+                            <li class="text-md leading-7 md:leading-relaxed">
                                 {{ __("content.software_engineer.section_2.requirements.content_$stackListItem") }}
                             </li>
                         @endfor
