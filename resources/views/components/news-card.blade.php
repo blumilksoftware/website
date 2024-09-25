e33ae0df0d9120af18638e2df0e6503814c285df
dawidrudnik <dawid.rudnik@blumilk.pl>
2024-09-25 13:07:04 +0200

    #356 - wip

--- resources/views/components/news-card.blade.php
+++ resources/views/components/news-card.blade.php
@@ -8,15 +8,22 @@
             'lg:h-[340px]' => !$isFirst,
          ])>
         <a href="{{ route('news.entry', $news['slug']) }}">
-            <span class="absolute inset-0"></span>
-            <img src="{{ asset("storage/".$news['photo']) }}" alt="{{ $news['title'] }}" class="h-auto w-full lg:h-full object-cover aspect-square lg:aspect-auto">
+            @if($isFirst)
+                <img src="{{ asset("storage/".$news['photo']) }}"
+                     alt="{{ $news['title'] }}"
+                     class="absolute h-auto w-full lg:h-full object-cover aspect-square blur-md lg:aspect-auto">
+                <img src="{{ asset("storage/".$news['photo']) }}"
+                     alt="{{ $news['title'] }}"
+                     class="absolute h-auto w-full lg:h-full object-contain aspect-square lg:aspect-auto">
+            @else
+                <img src="{{ asset("storage/".$news['photo']) }}" alt="{{ $news['title'] }}" class="h-auto w-full lg:h-full object-cover aspect-square lg:aspect-auto">
+            @endif
         </a>
     </div>
     <div
          @class([
              'lg:absolute lg:bottom-0 lg:px-12 lg:pb-12 lg:pt-6 flex flex-col lg:w-[80%] lg:place-self-center gap-2 lg:gap-3 lg:bg-white'=>$isFirst,
              'flex flex-col gap-3' => !$isFirst,
-
          ])>
         <div class="flex text-sm pt-3 justify-between">
             <time datetime="{{ $news['published_at'] }}" class="text-gray-400 my-auto">
