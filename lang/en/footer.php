07d323576650d49c0c9af8dc51cde43a923a7786
krzysztofrewak <krzysztof.rewak@blumilk.pl>
2024-10-14 10:11:30 +0200

    - contact page and footer tweaks

--- lang/en/footer.php
+++ lang/en/footer.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 return [
+    "code" => "Source code",
     "policy" => "Privacy policy",
     "data" => " Company data",
 ];
--- lang/pl/footer.php
+++ lang/pl/footer.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 return [
+    "code" => "Kod źródłowy",
     "policy" => "Polityka prywatności",
     "data" => " Dane firmy",
 ];
