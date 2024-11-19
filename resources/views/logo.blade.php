e383507387ff55ad1501c7002b1771479b47c537
EwelinaSkrzypacz <ewelina.skrzypacz@blumilk.pl>
2024-11-19 10:53:55 +0100

    - changed '' to ""

--- resources/views/logo.blade.php
+++ resources/views/logo.blade.php
@@ -1,21 +1,21 @@
 @extends("layout.public", ["excludeBigBubbles" => true])
 
 @php
-    $filesDirectory = public_path('materials');
+    $filesDirectory = public_path("materials");
     $materials = [];
 
     if (file_exists($filesDirectory) && is_dir($filesDirectory)) {
         foreach (scandir($filesDirectory) as $file) {
-            if (!in_array($file, ['.', '..'])) {
+            if (!in_array($file, [".", ".."])) {
                 $fileKey = pathinfo($file, PATHINFO_FILENAME);
-                $filePath = $filesDirectory . '/' . $file;
+                $filePath = $filesDirectory . "/" . $file;
 
                 $materials[] = [
-                    'title' => __("materials.{$fileKey}.title"),
-                    'description' => __("materials.{$fileKey}.description"),
-                    'file_type' => pathinfo($file, PATHINFO_EXTENSION),
-                    'file_size' => number_format(filesize($filePath) / 1048576, 2) . ' MB',
-                    'download_url' => asset('materials/' . $file),
+                    "title" => __("materials.{$fileKey}.title"),
+                    "description" => __("materials.{$fileKey}.description"),
+                    "file_type" => pathinfo($file, PATHINFO_EXTENSION),
+                    "file_size" => number_format(filesize($filePath) / 1048576, 2) . " MB",
+                    "download_url" => asset("materials/" . $file),
                 ];
             }
         }
@@ -40,17 +40,17 @@
                     <div class="relative w-[220px] md:w-[180px] h-[150px] sm:h-[180px] md:h-[105px] bg-gray-100 flex items-center justify-center rounded-md md:mr-4">
                         <i class="ti ti-paperclip text-website-normal -rotate-45 text-xl"></i>
                         <span class="absolute bottom-2 left-2 bg-website-normal text-white text-[10px] font-medium px-2 py-1">
-                            {{ $material['file_type'] }}, {{ $material['file_size'] }}
+                            {{ $material["file_type"] }}, {{ $material["file_size"] }}
                         </span>
                     </div>
                     <div class="text-center md:text-left space-y-2 md:space-y-3">
-                        <h2 class="text-lg font-semibold text-gray-800">{{ $material['title'] }}</h2>
-                        <p class="text-sm text-gray-500">{{ $material['description'] }}</p>
+                        <h2 class="text-lg font-semibold text-gray-800">{{ $material["title"] }}</h2>
+                        <p class="text-sm text-gray-500">{{ $material["description"] }}</p>
                     </div>
                 </div>
                 <x-small-primary-button href="{{ $material['download_url'] }}" class="font-semibold text-sm w-full md:w-auto">
                     <span>{{ __("buttons.download") }}</span>
-                    <span class="sr-only">{{ __('alt.download') }}</span>
+                    <span class="sr-only">{{ __("alt.download") }}</span>
                     <i class="ti ti-download text-lg text-white ml-2 font-semibold"></i>
                 </x-small-primary-button>
             </div>
