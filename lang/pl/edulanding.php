ac87b5d578f4e2cf275fd812e21079f6b15f350b
EwelinaSkrzypacz <ewelina.skrzypacz@blumilk.pl>
2025-06-25 11:11:32 +0200

    - intro section

--- lang/pl/edulanding.php
+++ lang/pl/edulanding.php
@@ -16,4 +16,22 @@ return [
         "description" => "Przyszłość edukacji zaczyna się od dobrze zaprojektowanego systemu. Tworzymy narzędzia, które realnie usprawniają codzienne funkcjonowanie placówek oświatowych.",
         "button" => "Dowiedz się więcej",
     ],
+    "intro_section" => [
+        "title_1" => "Dedykowane",
+        "title_2" => "rozwiązania IT",
+        "title_3" => "dla uczelni, szkół i instytucji edukacyjnych",
+        "description" => "Tworzymy systemy, które realnie usprawniają codzienne funkcjonowanie uczelni - od automatyzacji procesów po bezpieczeństwo danych.",
+        "tile_1" => [
+            "title" => "Skrócenie czasu realizacji procesów",
+            "description" => "Dzięki cyfryzacji i automatyzacji kluczowych zadań administracyjnych (np. rekrutacji, zarządzania oprogramowaniem, obiegu dokumentów), czas realizacji pojedynczego procesu skraca się nawet o 80%. Pracownicy mogą szybciej reagować na potrzeby studentów i kadry.",
+        ],
+        "tile_2" => [
+            "title" => "Wzrost wydajności operacyjnej",
+            "description" => "Wyeliminowanie ręcznego wprowadzania danych, podwójnych wpisów i zbędnych etapów pozwala uczelniom osiągnąć wzrost efektywności operacyjnej nawet o 70%. Pracownicy wykonują więcej zadań w krótszym czasie — bez utraty jakości.",
+        ],
+        "tile_3" => [
+            "title" => "Ograniczenie dokumentacji papierowej",
+            "description" => "Cyfrowa transformacja dokumentów i pełna archiwizacja elektroniczna umożliwiają ograniczenie użycia papieru nawet o 90%. To nie tylko oszczędność materiałów i przestrzeni, ale też szybszy dostęp do danych i zgodność z przepisami RODO.",
+        ],
+    ],
 ];
--- resources/views/edulanding/edu-landing.blade.php
+++ resources/views/edulanding/edu-landing.blade.php
@@ -67,8 +67,8 @@
                     <p class="text-sm sm:text-lg text-center xl:text-start pb-8 tablet:pb-16 text-edu-gray">
                         <span class="leading-7"> {{ __("edulanding.hero_section.description") }}</span>
                     </p>
-                    <x-edulanding.button href="#"
-                                      class="w-full sm:w-auto">{{ __("edulanding.hero_section.button") }}
+                    <x-edulanding.button href="#contact"
+                                      class="w-full sm:w-auto">{{ __("edulanding.hero_section.button")}}
                         <i class="ti ti-arrow-right"></i>
                     </x-edulanding.button>
                 </div>
@@ -77,6 +77,34 @@
                 <img src="{{ asset('images/edulanding/plus-icons.svg') }}" class="absolute z-0 hidden xl:block xl:bottom-[-100px] xl:right-[-60px]" alt=""/>
                 <img src="{{ asset('images/edulanding/hero-section.webp') }}" class="h-[250px] w-[350px] tablet:h-[400px] tablet:w-[600px] lg:h-[500px] lg:w-[750px] aspect-auto object-center rounded-[32px] z-10"
                      alt=""/>
+            </div>
+        </section>
+        <section class="relative">
+            <div class="mx-[10%] 2xl:max-w-[1200px] 2xl:mx-auto space-y-10 pt-8 sm:pt-20 lg:pt-40 lg:pb-28">
+                <div class="pb-6 flex text-center flex-col items-center gap-y-6">
+                    <div class="text-3xl md:text-5xl lg:text-6xl text-center">
+                        <h2 class="font-semibold text-gray-900 leading-7 md:leading-tight">
+                            <span class="inline">{{ __("edulanding.intro_section.title_1") }}</span>
+                            <span class="text-edu-normal inline"> {{ __("edulanding.intro_section.title_2") }} </span>
+                            <span class="inline">{{ __("edulanding.intro_section.title_3") }}</span>
+                        </h2>
+                    </div>
+                    <div class="font-light leading-7 text-sm sm:text-md lg:text-lg text-edu-gray max-w-2xl">{{ __("edulanding.intro_section.description") }}</div>
+                </div>
+                <div class="grid xl:grid-cols-3 gap-4 2xl:gap-12 xl:place-items-stretch">
+                    <x-edulanding.tile title="{{ __('edulanding.intro_section.tile_1.title') }}"
+                                       description="{{ __('edulanding.intro_section.tile_1.description') }}">
+                        <x-edulanding.progress-circle :value="80" />
+                    </x-edulanding.tile>
+                    <x-edulanding.tile title="{{ __('edulanding.intro_section.tile_2.title') }}"
+                            description="{{ __('edulanding.intro_section.tile_2.description') }}">
+                        <x-edulanding.progress-circle :value="70" />
+                    </x-edulanding.tile>
+                    <x-edulanding.tile title="{{ __('edulanding.intro_section.tile_3.title') }}"
+                                       description="{{ __('edulanding.intro_section.tile_3.description') }}">
+                        <x-edulanding.progress-circle :value="90" />
+                    </x-edulanding.tile>
+                </div>
             </div>
         </section>
     </section>
