1e0b1be84d955f9bffacc54beb79a9f17516d6ee
EwelinaSkrzypacz <ewelina.skrzypacz@blumilk.pl>
2026-07-24 13:15:29 +0200

    - cleaning

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,30 +12,6 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call(LocalEnvironmentSeeder::class);
-        $this->seedEmployeeRequests();
 
-    }
-
-    private function seedEmployeeRequests(): void
-    {
-        Project::factory()->create([
-            "name_first_part" => [
-                "pl" => "System do zarządzania wnioskami zgłaszanymi przez pracowników firmy",
-                "en" => "A system for managing requests submitted by company employees",
-            ],
-            "name_second_part" => [
-                "pl" => null,
-                "en" => null,
-            ],
-            "name_third_part" => [
-                "pl" => null,
-                "en" => null,
-            ],
-            "color" => "#000000",
-            "photo" => "factory/case_study.jpg",
-            "published" => true,
-            "slug" => "employee-requests",
-            "template" => "employee-requests",
-        ]);
     }
 }
