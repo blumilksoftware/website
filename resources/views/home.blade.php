20e6ed49b321efd73d0d16170d759212b6d651bb
EwelinaSkrzypacz <ewelina.skrzypacz@blumilk.pl>
2026-08-06 09:22:27 +0200

    - cr fixes - references

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -172,9 +172,10 @@
                 <div class="relative h-[600px] overflow-y-hidden px-4 mask-[linear-gradient(to_bottom,transparent,black_8%,black_92%,transparent)]">
                     <div class="flex flex-col gap-4 animate-scroll-vertical"
                          x-data="{}"
-                         @touchstart.passive="$el.classList.add('paused')"
-                         @touchend="$el.classList.remove('paused')"
-                         @touchcancel="$el.classList.remove('paused')">
+                         @pointerdown="$el.classList.add('paused')"
+                         @pointerup="$el.classList.remove('paused')"
+                         @pointercancel="$el.classList.remove('paused')"
+                         @pointerleave="$el.classList.remove('paused')">
                         @foreach ($references as $reference)
                             <x-references.review-card :reference="$reference"/>
                         @endforeach
